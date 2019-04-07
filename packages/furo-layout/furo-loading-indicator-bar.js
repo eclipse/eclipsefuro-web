47ded7f48d9b71d8231bc2b2f4c6db88cabbd30a
veith <veithz@gmail.com>
2019-04-07 15:06:55 +0200

    feat: furo-loading-indicator-bar.js init
    Signed-off-by: veith <veithz@gmail.com>

---
+++ packages/furo-layout/furo-loading-indicator-bar.js
@@ -0,0 +1,76 @@
+import {LitElement, html, css} from 'lit-element';
+import {FBP} from "@furo/fbp";
+
+/**
+ * `loading-indicator`
+ * Describe your element
+ * todo: make it like https://material.io/design/components/progress-indicators.html#
+ *
+ * @summary shortdescription
+ * @customElement
+ * @demo demo/loading-indicator.html
+ * @appliesMixin FBP
+ */
+class FuroLoadingIndicatorBar extends FBP(LitElement) {
+
+  constructor() {
+    super();
+    this.setAttribute("hidden", "")
+  }
+
+
+  /**
+   *
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return css`
+        :host {
+            display: block;
+            width: 100%;
+            height: 4px;
+            pointer-events: none;
+            opacity: 0.8;
+            background-color: var(--secondary-color);
+            animation: colorchange 5s;
+            @apply --loading-indicator-mixin;
+        }
+
+        :host([hidden]) {
+            display: none;
+        }
+
+        @keyframes colorchange
+        {
+            0%   {background: red;}
+            25%  {background: yellow;}
+            50%  {background: blue;}
+            75%  {background: green;}
+            100% {background: red;}
+        }
+    `
+  }
+
+  start() {
+    this.removeAttribute("hidden");
+  }
+
+  stop() {
+    this.setAttribute("hidden", "");
+  }
+
+  /**
+   * @private
+   * @returns {TemplateResult}
+   */
+  render() {
+    // language=HTML
+    return html`
+      <slot></slot>
+    `;
+  }
+}
+
+window.customElements.define('furo-loading-indicator-bar', FuroLoadingIndicatorBar);
