e32b1c9540a0609c0b54c49d90518ac2836ec58b
veith <veithz@gmail.com>
2019-04-22 12:21:32 +0200

    chore: add todos for furo-date-input
    Signed-off-by: veith <veithz@gmail.com>

--- packages/furo-input/furo-date-input.js
+++ packages/furo-input/furo-date-input.js
@@ -43,8 +43,8 @@ ${this._sharedStyle}
   bindData(d) {
 
     // capture defaults
+    // todo eow, eom, bow, bow, ...
     if (d._meta.default === "today") {
-
       let today = new Date();
       let dd = String(today.getDate()).padStart(2, '0');
       let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
