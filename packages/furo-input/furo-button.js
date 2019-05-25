a6a70d3785b3d35a794a61493d84b2f23fca1ddd
veith <veithz@gmail.com>
2019-05-25 10:48:02 +0200

    feat: furo-button as lit element, with disabled, autofocus and danger

--- packages/furo-input/furo-button.js
+++ packages/furo-input/furo-button.js
@@ -1,148 +1,151 @@
-import {PolymerElement, html} from '@polymer/polymer';
+import {LitElement, html, css} from 'lit-element';
+import {Theme} from "@furo/framework/theme"
 import {FBP} from "@furo/fbp";
-import "@polymer/paper-ripple/paper-ripple"
+import "@polymer/paper-ripple"
 
 /**
  * `furo-button`
+ * todo Describe your element
  *
- * Simple button element which uses a native `<button>` tag with paper-ripple
- *
- * Tags: input
- * @summary button element
+ * @summary todo shortdescription
  * @customElement
- * @polymer
- * @mixes FBP
+ * @demo demo/furo-button.html
+ * @appliesMixin FBP
  */
-class FuroButton extends FBP(PolymerElement) {
-  static get template() {
-    // language=HTML
-    return html`
-      <style >
+class FuroButton extends FBP(LitElement) {
 
-        :host {
-          display: inline-block;
-          position: relative;
-          font-size: 16px;
-          box-sizing: border-box;
-          margin: 0;
-          padding: 8px 0;
-        }
-        input {
-          border: none;
-          border-bottom: 1px solid rgba(0, 0, 0, .12);
-          display: block;
-          background: 0 0;
-          font-size: 12px;
-          margin: 0;
-          padding: 4px 0;
-          width: 100%;
-          text-align: left;
-          color: inherit;
-          outline: none;
-        }
-
-        input:focus {
-          border-color: var(--app-primary-color,#3f51b5);
-          border-width: 1px;
-        }
-
-        label {
-          bottom: 0;
-          color: rgba(0, 0, 0, .26);
-          font-size: 12px;
-          left: 0;
-          right: 0;
-          pointer-events: none;
-          position: absolute;
-          display: block;
-          top: 12px;
-          width: 100%;
-          overflow: hidden;
-          white-space: nowrap;
-          text-align: left;
-        }
-
-        label[float] {
-          color: #3f51b5;
-          font-size: 10px;
-          top: -4px;
-          visibility: visible;
-        }
-
-        * {
-          transition: all 200ms ease-in;
-        }
-        .hint{
-          position: absolute;
-          bottom: -7px;
-          font-size: 10px;
-          color:transparent;
-
-        }
-        :host(:focus-within) .hint{
-          color: var(--app-hint-color);
-          transition: all 750ms ease-in;
-        }
-        
-        button {
-          position: relative;
-          background-color: #FFFFFF;
-          border: 1px solid gainsboro;
-          border-radius: 2px;
-          width: 100%;
-          cursor: pointer;
-        }
-
-        button:focus {
-          outline: none;
-          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
-          @apply --furo-button-focus-mixin;
-        }
-
-        :host([danger]) button:focus {
-          @apply --furo-button-danger-focus-mixin;
-        }
-        
-        :host([danger]) button {
-          @apply --furo-button-danger-mixin;
-        } 
-        :host([hidden]) button {
-          display: none;
-        }
-        
-      </style>
-
-      <button  ƒ-focus="--focus" danger$="[[danger]]">[[label]]
-        <slot></slot>
-        <paper-ripple></paper-ripple>
-      </button>
-    `;
+  constructor() {
+    super();
+    this.label = "label not set";
+    this.disabled = false;
+    this.danger = false;
   }
 
   focus(e) {
     this._FBPTriggerWire("--focus", e);
   }
 
+  /**
+   * @private
+   * @return {Object}
+   */
   static get properties() {
     return {
       /**
        * Beschriftung des buttons
        */
       label: {
-        type: String,
-        value: "label not set",
+        type: String
       },
       /**
        * Set danger to true if it is dangerous to press this button
        */
       danger: {
         type: Boolean,
-        reflectToAttribute: true,
-        value: false,
+        reflect: true
       },
+      /**
+       * Set disabled to disable the button
+       */
+      disabled: {
+        type: Boolean,
+        reflect: true
+      },
+      /**
+       * Set danger to true if it is dangerous to press this button
+       */
+      autofocus: {
+        type: Boolean,
+        reflect: true
+      }
     };
   }
 
+
+  /**
+   *
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return Theme.getThemeForComponent(this.name) || css`
+        :host {
+            display: block;
+        }
+
+        :host([hidden]) {
+            display: none;
+        }
+
+        :host {
+            display: inline-block;
+            position: relative;
+            font-size: 16px;
+            box-sizing: border-box;
+            margin: 0;
+            padding: 8px 0;
+        }
+
+        * {
+            transition: all 200ms ease-in;
+        }
+
+        button {
+            position: relative;
+            border-radius: 2px;
+            width: 100%;
+            cursor: pointer;
+            background-color: var(--primary-color);
+            color: var(--on-primary);
+            padding: 12px 24px;
+            border: none;
+            text-transform: uppercase;
+            font-size: 14px;
+        }
+
+        button:focus {
+            outline: none;
+            box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+            background-color: var(--secondary-color);
+            color: var(--on-secondary);
+        }
+
+        :host([danger]) button:focus {
+            background-color: lightgray;
+            box-sizing: border-box;
+            color: var(--danger-color);
+        }
+
+        button[disabled]  {
+            box-sizing: border-box;
+            background-color: var(--disabled-color,#eeeeee);
+            color: var(--on-disabled,#333333);
+            
+        }
+
+        :host([danger]) button {
+            background-color: var(--danger-color, #ff0000);
+            color: var(--on-danger, #FFFFFF);
+        }
+
+    `
+  }
+
+
+  /**
+   * @private
+   * @returns {TemplateResult}
+   */
+  render() {
+    // language=HTML
+    return html`
+      <button ƒ-focus="--focus" ?autofocus=${this.autofocus} ?disabled=${this.disabled} ?danger=${this.danger}>${this.label}
+        <slot></slot>
+        <paper-ripple></paper-ripple>
+      </button>
+    `;
+  }
 }
 
 window.customElements.define('furo-button', FuroButton);
