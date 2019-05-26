92607caf36f56101fe910e365efb4f02a54598ac
veith <veithz@gmail.com>
2019-05-26 19:26:33 +0200

    feat: vertical-flex with user agent style only

--- packages/furo-layout/furo-vertical-flex.js
+++ packages/furo-layout/furo-vertical-flex.js
@@ -29,50 +29,44 @@ class FuroVerticalFlex extends LitElement {
   }
 
 
-    /**
-     *
-     * @private
-     * @return {CSSResult}
-     */
-    static get styles() {
-      // language=CSS
-      return Theme.getThemeForComponent(this.name) || css`
-          :host {
-              --layout: {
-                  display: -ms-flexbox;
-                  display: -webkit-flex;
-                  display: flex;
-              };
+  /**
+   *
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return Theme.getThemeForComponent(this.name) || css`        
+        :host {
+            display: block;
+            height: 100%;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -ms-flex-direction: column;
+            -webkit-flex-direction: column;
+            flex-direction: column;
+        }
 
-              --layout-vertical: {
-                  @apply --layout;
-                  -ms-flex-direction: row;
-                  -webkit-flex-direction: row;
-                  flex-direction: row;
-              };
+        :host([hidden]) {
+            display: none;
+        }
 
-              --layout-vertical: {
-                  @apply --layout;
-
-                  -ms-flex-direction: column;
-                  -webkit-flex-direction: column;
-                  flex-direction: column;
-              };
-
-              --layout-vertical-reverse: {
-                  @apply --layout;
-
-                  -ms-flex-direction: column-reverse;
-                  -webkit-flex-direction: column-reverse;
-                  flex-direction: column-reverse;
-              };
-          }
-          :host([hidden]) {
-              display: none;
-          }
-      `
-    }
-
+        :host([reverse]) {
+            -ms-flex-direction: column-reverse;
+            -webkit-flex-direction: column-reverse;
+            flex-direction: column-reverse;
+        }
+        
+        ::slotted(*[flex]) {
+            -ms-flex: 1 1 0.000000001px;
+            -webkit-flex: 1;
+            flex: 1;
+            -webkit-flex-basis: 0.000000001px;
+            flex-basis: 0.000000001px;
+        }
+    `
+  }
 
 
   /**
@@ -81,17 +75,7 @@ class FuroVerticalFlex extends LitElement {
    */
   render() {
     // language=HTML
-    return html`  
-        <style>
-          :host{
-            display: block;
-            height: 100%;
-            @apply --layout-vertical;
-          }
-          ::slotted(*[flex]) {
-            @apply --layout-flex;
-          }
-        </style>
+    return html`        
       <slot></slot>
     `;
   }
