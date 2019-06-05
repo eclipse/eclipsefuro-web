e69b6e2298cc255263720ec57ae3f32734ac33a5
veith <veithz@gmail.com>
2019-06-05 07:24:37 +0200

    fix: furo-pages works with location object from furo-location

--- packages/furo-route/furo-pages.js
+++ packages/furo-route/furo-pages.js
@@ -36,62 +36,7 @@ class FuroPages extends (LitElement) {
    * @param String pageName
    */
   activatePage(pageName) {
-
-    if (!pageName) {
-      pageName = this._fallback;
-    }
-    if (this._lastPage && pageName !== this._lastPageName) {
-      if (this._lastPage._FBPTriggerWire !== undefined) {
-        this._lastPage._FBPTriggerWire('--pageDeActivated');
-      }
-      this._lastPage.setAttribute("hidden", '');
-      this._lastPage.removeAttribute(this._attrForSelected)
-    }
-
-    this._lastPage = this.querySelector("*[name=" + pageName + "]");
-
-    if (!this._lastPage) {
-      // 404
-      this._lastPage = this.querySelector('*[name="404"]');
-      if (this._lastPage) {
-      } else {
-        this._lastPage = this._default;
-      }
-    }
-    if (this._lastPage) {
-
-      if (pageName !== this._lastPageName) {
-        this._lastPage.removeAttribute("hidden");
-        this._lastPage.setAttribute(this._attrForSelected, '');
-
-        this._lastPageName = pageName;
-        if (this._lastPage._FBPTriggerWire !== undefined) {
-          this._lastPage._FBPTriggerWire('--pageActivated', location);
-        }
-
-      }
-
-      // QP
-      if (this._lastQP[pageName] !== location.querystring) {
-        this._lastQP[pageName] = location.querystring;
-        // fire --pageParamsChanged if we have a fbp component
-        if (this._lastPage._FBPTriggerWire !== undefined) {
-          this._lastPage._FBPTriggerWire('--pageQueryChanged', location);
-        }
-      }
-
-      // Hash
-      if (this._lastHash[pageName] !== location.hashstring) {
-        this._lastHash[pageName] = location.hashstring;
-        // fire --pageParamsChanged if we have a fbp component
-        if (this._lastPage._FBPTriggerWire !== undefined) {
-          this._lastPage._FBPTriggerWire('--pageHashChanged', location);
-        }
-      }
-
-    } else {
-      console.warn("default page not found and 404 page not found");
-    }
+    this.injectLocation({"pathSegments": [pageName]})
   }
 
   /**
@@ -111,9 +56,59 @@ class FuroPages extends (LitElement) {
   injectLocation(location) {
 
     let page = location.pathSegments[0] || this._fallback;
-    this.activatePage(page);
-    console.warn("Deprecated method: inject-location \n please use activate-page with the head-tail component.")
 
+    if (this._lastPage && page !== this._lastPageName) {
+      if (this._lastPage._FBPTriggerWire !== undefined) {
+        this._lastPage._FBPTriggerWire('--pageDeActivated');
+      }
+      this._lastPage.setAttribute("hidden", '');
+      this._lastPage.removeAttribute(this._attrForSelected)
+    }
+
+    this._lastPage = this.querySelector("*[name=" + page + "]");
+
+    if (!this._lastPage) {
+      // 404
+      this._lastPage = this.querySelector('*[name="404"]');
+      if (this._lastPage) {
+      } else {
+        this._lastPage = this._default;
+      }
+    }
+    if (this._lastPage) {
+
+      if (page !== this._lastPageName) {
+        this._lastPage.removeAttribute("hidden");
+        this._lastPage.setAttribute(this._attrForSelected, '');
+
+        this._lastPageName = page;
+        if (this._lastPage._FBPTriggerWire !== undefined) {
+          this._lastPage._FBPTriggerWire('--pageActivated', location);
+        }
+
+      }
+
+      // QP
+      if (this._lastQP[page] !== location.querystring) {
+        this._lastQP[page] = location.querystring;
+        // fire --pageParamsChanged if we have a fbp component
+        if (this._lastPage._FBPTriggerWire !== undefined) {
+          this._lastPage._FBPTriggerWire('--pageQueryChanged', location);
+        }
+      }
+
+      // Hash
+      if (this._lastHash[page] !== location.hashstring) {
+        this._lastHash[page] = location.hashstring;
+        // fire --pageParamsChanged if we have a fbp component
+        if (this._lastPage._FBPTriggerWire !== undefined) {
+          this._lastPage._FBPTriggerWire('--pageHashChanged', location);
+        }
+      }
+
+    } else {
+      console.warn("default page not found and 404 page not found");
+    }
   }
 
 
