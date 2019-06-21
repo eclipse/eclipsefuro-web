62fd90e6163e8883992c3003460b0a2c61104015
veith <veithz@gmail.com>
2019-06-21 23:02:13 +0200

    feat: furo iconset AvIcons

--- packages/furo-layout/test/initEnv.js
+++ packages/furo-layout/test/initEnv.js
@@ -2,5 +2,7 @@
 
 import {Init} from "@furo/framework/furo.js";
 import {FuroBaseIcons} from "../iconsets/baseIcons";
+import {AvIcons} from "../iconsets/avIcons";
 import {Iconset} from "@furo/framework/furo.js";
 Iconset.registerIconset("default", FuroBaseIcons);
+Iconset.registerIconset("av", AvIcons);
