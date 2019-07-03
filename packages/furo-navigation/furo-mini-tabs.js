430050141025ff47093b7ba37e7697569a431862
veith <veithz@gmail.com>
2019-07-03 12:48:02 +0200

    chore: init mini tabs

---
+++ packages/furo-navigation/furo-mini-tabs.js
@@ -0,0 +1,69 @@
+import { LitElement, html, css } from 'lit-element';
+import {Theme} from "@furo/framework/theme"
+import {FBP} from "@furo/fbp";
+import  "@furo/fbp/flow-repeat";
+
+/**
+ * `furo-mini-tabs`
+ * Tabs
+ *
+ * @summary todo shortdescription
+ * @customElement
+ * @demo demo/furo-mini-tabs.html
+ * @appliesMixin FBP
+ */
+class FuroMiniTabs extends FBP(LitElement) {
+
+    constructor() {
+        super();
+    }
+
+    injectItems(nodeArray){
+      this._FBPTriggerWire("--itemsInjected");
+
+    }
+
+  /**
+  * flow is ready lifecycle method
+  */
+  __fbpReady(){
+    super.__fbpReady();
+    //this._FBPTraceWires()
+  }
+
+  /**
+   * Themable Styles
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return Theme.getThemeForComponent(this.name) || css`
+        :host {
+            display: block;
+        }
+
+        :host([hidden]) {
+            display: none;
+        }
+    `
+  }
+
+
+  /**
+   * @private
+   * @returns {TemplateResult}
+   */
+  render() {
+    // language=HTML
+    return html`
+      <flow-repeat ƒ-inject-items="--itemsInjected">
+        <template>
+          jkj
+        </template>
+      </flow-repeat>
+    `;
+  }
+}
+
+window.customElements.define('furo-mini-tabs', FuroMiniTabs);
