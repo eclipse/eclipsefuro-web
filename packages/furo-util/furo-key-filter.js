6068b377396b96c2b5de5f908ff3d545d4b5cf5c
veith <veithz@gmail.com>
2019-07-06 14:06:51 +0200

    feat: key filter init

---
+++ packages/furo-util/furo-key-filter.js
@@ -0,0 +1,103 @@
+import {LitElement, html, css} from 'lit-element';
+import {Theme} from "@furo/framework/theme"
+import {FBP} from "@furo/fbp";
+
+/**
+ * `furo-key-filter`
+ * Allows only defined key events to pass through
+ *
+ * ```
+ * <!-- note the asterisk on other-component keydown. Because filter needs the keyboard event. -->
+ * <other-component @-keydown="--keydown(*)"></other-component>
+ * <furo-key-filter ƒ-filter="--keydown" @-matched="--escapePressed" keys="Escape"></furo-key-filter>
+ * ```
+ *
+ *
+ * @summary keyboard event filter
+ * @customElement
+ * @demo demo/furo-key-filter.html
+ * @appliesMixin FBP
+ */
+class FuroKeyFilter extends FBP(LitElement) {
+
+    constructor() {
+        super();
+
+    }
+
+    /**
+     * Check the event and dispatch matched when the conditions are fulfilled.
+     *
+     * @param keyboardEvent
+     */
+    filter(keyboardEvent) {
+        let key = keyboardEvent.key || keyboardEvent.keyCode;
+
+        // check shift, alt, command,...
+        if (this.shift && !keyboardEvent.shiftKey) {
+            return
+        }
+        if (this.alt && !keyboardEvent.altKey) {
+            return
+        }
+        if ((this.meta || this.command) && !keyboardEvent.metaKey) {
+            return
+        }
+        if ((this.control) && !keyboardEvent.controlKey) {
+            return
+        }
+
+
+        if(this.keys.split(/\W+/).indexOf(key) !== -1){
+            /**
+            * @event matched
+            * Fired when key matches the options
+            * detail payload: keyboardEvent
+            */
+            let customEvent = new Event('matched', {composed:true, bubbles: true});
+            customEvent.detail = keyboardEvent;
+            this.dispatchEvent(customEvent)
+        }
+
+
+
+    }
+
+    /**
+     * @private
+     * @return {Object}
+     */
+    static get properties() {
+        return {
+            /**
+             * Coma separated list with allowed keys to pass. i.e "Enter, ArrowUp"
+             */
+            keys: {type: String},
+            /**
+             * Modifier key **shift** musst be pressed too to match
+             */
+            shift: {type: Boolean},
+            /**
+             * Modifier key **alt** musst be pressed too to match
+             */
+            alt: {type: Boolean},
+            /**
+             * Alias for meta.
+             *
+             * Modifier key **meta** musst be pressed too to match.
+             */
+            command: {type: Boolean},
+            /**
+             * Modifier key **meta** musst be pressed too to match
+             */
+            meta: {type: Boolean},
+            /**
+             * Modifier key **control** musst be pressed too to match
+             */
+            control: {type: Boolean}
+        };
+    }
+
+}
+
+window.customElements.define('furo-key-filter', FuroKeyFilter);
