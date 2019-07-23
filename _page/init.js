21bae9937f67db46745d5e28645b0008e6e044da
veith <veithz@gmail.com>
2019-07-23 10:29:45 +0200

    doc: init all icons

--- _page/init.js
+++ _page/init.js
@@ -12,10 +12,24 @@ import {PlacesIcons} from "@furo/layout/iconsets/placesIcons";
 import {CommunicationIcons} from "@furo/layout/iconsets/communicationIcons";
 import {NotificationIcons} from "@furo/layout/iconsets/notificationIcons";
 import {FuroDocIcons} from "./assets/iconset";
+import {AvIcons} from "@furo/layout/iconsets/avIcons";
+import {DeviceIcons} from "@furo/layout/iconsets/deviceIcons";
+import {EditorIcons} from "@furo/layout/iconsets/editorIcons";
+import {SocialIcons} from "@furo/layout/iconsets/socialIcons";
+import {HardwareIcons} from "@furo/layout/iconsets/hardwareIcons";
+import {ImageIcons} from "@furo/layout/iconsets/imageIcons";
+
+
+Iconset.registerIconset("furo", FuroDocIcons);
 
 Iconset.registerIconset("default", FuroBaseIcons);
-Iconset.registerIconset("maps", MapsIcons);
-Iconset.registerIconset("places", PlacesIcons);
-Iconset.registerIconset("furo", FuroDocIcons);
+Iconset.registerIconset("av", AvIcons);
 Iconset.registerIconset("communication", CommunicationIcons);
+Iconset.registerIconset("device", DeviceIcons);
+Iconset.registerIconset("editor", EditorIcons);
+Iconset.registerIconset("social", SocialIcons);
+Iconset.registerIconset("places", PlacesIcons);
 Iconset.registerIconset("notification", NotificationIcons);
+Iconset.registerIconset("map", MapsIcons);
+Iconset.registerIconset("hardware", HardwareIcons);
+Iconset.registerIconset("image", ImageIcons);
