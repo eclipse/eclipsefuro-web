59dbcb7b151d2fef3384842a9117d9c623070a8a
Zou Fan <fan.zou@gmail.com>
2019-08-23 14:25:31 +0200

    chore: demo for furo data checkbox input

--- packages/furo-data-input/demo/demo-furo-data-checkbox-input.js
+++ packages/furo-data-input/demo/demo-furo-data-checkbox-input.js
@@ -1,9 +1,13 @@
-import { LitElement, html, css } from 'lit-element';
+import {LitElement, html, css} from 'lit-element';
 import {Theme} from "@furo/framework/theme"
 import {FBP} from "@furo/fbp";
-import "@furo/doc-helper"
-import "@furo/data/entity-object"
-import "../furo-catalog"
+import "@furo/doc-helper";
+import "@furo/data/furo-data-object";
+import "@furo/data/deep-link";
+import "@furo/layout/furo-horizontal-flex";
+import "../furo-catalog";
+import "@furo/data/entity-agent";
+
 /**
  * `demo-furo-data-checkbox-input`
  *
@@ -11,6 +15,10 @@ import "../furo-catalog"
  * @appliesMixin FBP
  */
 class DemoFuroDataCheckboxInput extends FBP(LitElement) {
+
+    constructor() {
+        super();
+    }
 
     /**
      * Themable Styles
@@ -20,19 +28,18 @@ class DemoFuroDataCheckboxInput extends FBP(LitElement) {
     static get styles() {
         // language=CSS
         return Theme.getThemeForComponent(this.name) || css`
-        :host {
-            display: block;
-            height: 100%;
-            padding-right: var(--spacing);
-        }
+            :host {
+                display: block;
+                height: 100%;
+                padding-right: var(--spacing);
+            }
 
-        :host([hidden]) {
-            display: none;
-        }
-       
-    `
+            :host([hidden]) {
+                display: none;
+            }
+
+        `
     }
-
 
     /**
      * @private
@@ -41,20 +48,40 @@ class DemoFuroDataCheckboxInput extends FBP(LitElement) {
     render() {
         // language=HTML
         return html`
-      <h2>Demo furo-data-checkbox-input</h2>
-      <p>Bind the field from entity-object with <strong>ƒ-bind-data="--entityReady(*.fields.fieldname)"</strong>. The labels, hints, defaults are comming from the entity-object specs.</p>
-      <furo-demo-snippet >
-        <template>
-          <entity-object type="navigationnode" @-object-ready="--entity"></entity-object>
-          <furo-data-checkbox-input trailing-icon="dashboard" ƒ-bind-data="--entity(*.fields.open)"></furo-data-checkbox-input>
-          <furo-data-checkbox-input leading-icon="dashboard"  label="disabled" hint="disabled hint" disabled=true ƒ-bind-data="--entity(*.fields.open)"></furo-data-checkbox-input>
-          <furo-data-checkbox-input autofocus ƒ-bind-data="--entity(*.fields.open)" @-value-changed="--checkChanged" hint="the checked value will be sent to text input"></furo-data-checkbox-input>
-            <!-- --textChanged only comes when data was typed in. -->
-            <furo-text-input label="wire the checkbox" ƒ-set-value="--checkChanged"></furo-text-input>
-        </template>
-      </furo-demo-snippet>
-    `;
+            <h2>Demo furo-data-checkbox-input</h2>
+            <p>Bind the field from entity-object with <strong>ƒ-bind-data="--entityReady(*.fields.fieldname)"</strong>.
+                The labels, hints, defaults are comming from the entity-object specs.</p>
+            <furo-demo-snippet>
+                <template>
+                    <furo-data-checkbox-input trailing-icon="dashboard"
+                                              ƒ-bind-data="--entity(*.fields.open)"></furo-data-checkbox-input>
+                    <furo-data-checkbox-input leading-icon="dashboard" label="disabled" hint="disabled hint"
+                                              disabled=true
+                                              ƒ-bind-data="--entity(*.fields.open)"></furo-data-checkbox-input>
+                    <furo-horizontal-flex space>
+
+                        <furo-data-checkbox-input style="margin-top:12px" autofocus ƒ-bind-data="--entity(*.furo_data_checkbox_input)"
+                                                  @-value-changed="--checkChanged"
+                                                  hint="the checked value will be sent to text input"></furo-data-checkbox-input>
+
+                        <furo-text-input condensed label="wire the checkbox" ƒ-set-value="--checkChanged"></furo-text-input>
+                        <furo-button style="margin-top:18px" @-click='--qpIn' label="load test data"  raised></furo-button>
+                    </furo-horizontal-flex>
+
+
+                    <furo-data-object type="experiment.Experiment" @-data-injected="--entity"
+                                      ƒ-inject-raw="--response(*.data)"></furo-data-object>
+                    <deep-link service="ExperimentService" @-hts-out="--hts" ƒ-trigger="--qpIn" qp='{"exp": 1}'></deep-link>
+                    <entity-agent service="ExperimentService"
+                                  ƒ-hts-in="--hts"
+                                  ƒ-load="--hts"
+                                  ƒ-bind-request-data="--entity"
+                                  @-response="--response">
+                    </entity-agent>
+                </template>
+            </furo-demo-snippet>
+        `;
     }
 }
 
-window.customElements.define('demo-furo-data-checkbox-input', DemoFuroDataCheckboxInput );
+window.customElements.define('demo-furo-data-checkbox-input', DemoFuroDataCheckboxInput);
