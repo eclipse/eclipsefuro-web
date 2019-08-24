e7f5c849092c12a2bf582bfc77446a9fd72da1b3
veith <veithz@gmail.com>
2019-08-24 18:30:41 +0200

    fix: paths in prepare when running as a node_module

--- packages/furo-specs/_scripts/prepare.sh
+++ packages/furo-specs/_scripts/prepare.sh
@@ -6,34 +6,34 @@ SPECDIR=$1
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mkdir -p $DIR/../__tmp
+mkdir -p ./__tmp
 
-BASETYPEDIR="$DIR/../_baseTypes"
+BASETYPEDIR="./_baseTypes"
 
-rm $DIR/../__tmp/types.json
+rm ./__tmp/types.json
 # open Array
-echo '{"types":[' >> $DIR/../__tmp/types.json
+echo '{"types":[' >> ./__tmp/types.json
 
-for t in `find $SPECDIR -name '*.type.spec'`; do (cat $t; echo ',') >> $DIR/../__tmp/tmptypes_s.json; done
-for t in `find $BASETYPEDIR -name '*.type.spec'`; do (cat $t; echo ',') >> $DIR/../__tmp/tmptypes_s.json; done
+for t in `find $SPECDIR -name '*.type.spec'`; do (cat $t; echo ',') >> ./__tmp/tmptypes_s.json; done
+for t in `find $BASETYPEDIR -name '*.type.spec'`; do (cat $t; echo ',') >> ./__tmp/tmptypes_s.json; done
 
-cat $DIR/../__tmp/tmptypes_s.json | sed '$ s/.$//' >> $DIR/../__tmp/types.json
-rm $DIR/../__tmp/tmptypes_s.json
+cat ./__tmp/tmptypes_s.json | sed '$ s/.$//' >> ./__tmp/types.json
+rm ./__tmp/tmptypes_s.json
 # close Array
-echo ']}' >> $DIR/../__tmp/types.json
+echo ']}' >> ./__tmp/types.json
 
 echo "single types done."
 
 
-rm $DIR/../__tmp/services.json
+rm ./__tmp/services.json
 # open Array
-echo '{"services":[' >> $DIR/../__tmp/services.json
+echo '{"services":[' >> ./__tmp/services.json
 
-for t in `find $SPECDIR -name '*.service.spec'`; do (cat $t; echo ',') >> $DIR/../__tmp/tmpservices.json; done
-cat $DIR/../__tmp/tmpservices.json | sed '$ s/.$//' >> $DIR/../__tmp/services.json
-rm $DIR/../__tmp/tmpservices.json
+for t in `find $SPECDIR -name '*.service.spec'`; do (cat $t; echo ',') >> ./__tmp/tmpservices.json; done
+cat ./__tmp/tmpservices.json | sed '$ s/.$//' >> ./__tmp/services.json
+rm ./__tmp/tmpservices.json
 # close Array
-echo ']}' >> $DIR/../__tmp/services.json
+echo ']}' >> ./__tmp/services.json
 
 echo "single services done."
 
--- packages/furo-specs/_scripts/prepareBundled.sh
+++ packages/furo-specs/_scripts/prepareBundled.sh
@@ -5,33 +5,33 @@ echo "*** Prepare ***"
 SPECDIR=$1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mkdir -p $DIR/../__tmp
-BASETYPEDIR="$DIR/../_baseTypes"
+mkdir -p ./__tmp
+BASETYPEDIR="./_baseTypes"
 
-rm $DIR/../__tmp/types_bundled.json
+rm ./__tmp/types_bundled.json
 # open Array
-echo '{"types":[' >> $DIR/../__tmp/types_bundled.json
+echo '{"types":[' >> ./__tmp/types_bundled.json
 
-for t in `find $SPECDIR -name '*.type.spec'`; do (cat $t; echo ',') >> $DIR/../__tmp/tmptypes.json; done
-#for t in `find $BASETYPEDIR -name '*.type.spec'`; do (cat $t; echo ',') >> $DIR/../__tmp/tmptypes.json; done
-cat $DIR/../__tmp/tmptypes.json | sed '$ s/.$//' >> $DIR/../__tmp/types_bundled.json
-rm $DIR/../__tmp/tmptypes.json
+for t in `find $SPECDIR -name '*.type.spec'`; do (cat $t; echo ',') >> ./__tmp/tmptypes.json; done
+#for t in `find $BASETYPEDIR -name '*.type.spec'`; do (cat $t; echo ',') >> ./__tmp/tmptypes.json; done
+cat ./__tmp/tmptypes.json | sed '$ s/.$//' >> ./__tmp/types_bundled.json
+rm ./__tmp/tmptypes.json
 # close Array
-echo ']}' >> $DIR/../__tmp/types_bundled.json
+echo ']}' >> ./__tmp/types_bundled.json
 
 echo "bundled types done."
 
 
-rm $DIR/../__tmp/services_bundled.json
+rm ./__tmp/services_bundled.json
 # open Array
-echo '{"services":[' >> $DIR/../__tmp/services_bundled.json
+echo '{"services":[' >> ./__tmp/services_bundled.json
 
-for t in `find $SPECDIR -name '*.service.spec'`; do (cat $t; echo ',') >> $DIR/../__tmp/tmpservices.json; done
+for t in `find $SPECDIR -name '*.service.spec'`; do (cat $t; echo ',') >> ./__tmp/tmpservices.json; done
 echo $t
-cat $DIR/../__tmp/tmpservices.json | sed '$ s/.$//' >> $DIR/../__tmp/services_bundled.json
-rm $DIR/../__tmp/tmpservices.json
+cat ./__tmp/tmpservices.json | sed '$ s/.$//' >> ./__tmp/services_bundled.json
+rm ./__tmp/tmpservices.json
 # close Array
-echo ']}' >> $DIR/../__tmp/services_bundled.json
+echo ']}' >> ./__tmp/services_bundled.json
 
 echo "bundled services done."
 
