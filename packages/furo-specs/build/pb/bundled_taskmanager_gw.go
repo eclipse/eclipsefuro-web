33ef828f0c427723ac1582036b64bf8fb2d6dd07
veith <veithz@gmail.com>
2019-08-30 18:31:21 +0200

    test: coverage report
    =============================== Coverage summary ===============================
    Statements   : 59.7% ( 2148/3598 )
    Branches     : 51.41% ( 768/1494 )
    Functions    : 63.91% ( 602/942 )
    Lines        : 59.71% ( 2128/3564 )
    ================================================================================

---
+++ packages/furo-specs/build/pb/bundled_taskmanager_gw.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	 "./__bundled"
+	"flag"
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	_ "google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	grpcserver = flag.String("grpcserver", os.Getenv("GRPC_SERVER_ADDRESS"), "grpc server for your services");
+	gwserver = flag.String("gwserver", os.Getenv("GW_SERVER_ADDRESS"), "Address and port for the gateway");
+)
+// header für client ohne prefixes
+func outgoingMatcher(headerName string) (mdName string, ok bool) {
+	return headerName, true
+}
+
+// header vom client im ctx des Servers ohne prefixes senden
+func incomingMatcher(headerName string) (mdName string, ok bool) {
+	return headerName, true
+}
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux(runtime.WithOutgoingHeaderMatcher(outgoingMatcher), runtime.WithIncomingHeaderMatcher(incomingMatcher))
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	// Services hier einfügen
+	// PersonService
+	err := taskmanager.RegisterBundledServiceHandlerFromEndpoint(ctx, mux, *grpcserver, opts)
+	if err != nil {
+		return err
+	}
+	
+	return http.ListenAndServe(*gwserver, mux)
+}
+
+func main() {
+	
+	flag.Parse()
+	defer glog.Flush()
+
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Println("Server is starting...")
+	logger.Printf("Connecting to " + *grpcserver + " and listening on: " + *gwserver);
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
