991e7bae24cac3476f4d02e09d9ed31b0cdd7c3b
veith <veithz@gmail.com>
2019-09-11 13:24:50 +0200

    publish 0.14.0

--- packages/furo-specs/build/ui/registry.js
+++ packages/furo-specs/build/ui/registry.js
@@ -11,22 +11,27 @@ import "./panels/tree-tree-update-panel"
 // -- register panels
 
 panelRegistry.registerType("experiment.Experiment", {
-     "edit" : "experiment-experiment-update-panel" ,"view" : "experiment-experiment-update-panel"
- });
+  "edit": "experiment-experiment-update-panel",
+  "view": "experiment-experiment-update-panel"
+});
 
 panelRegistry.registerType("person.Person", {
-     "edit" : "person-person-update-panel" ,"view" : "person-person-update-panel"
- });
+  "edit": "person-person-update-panel",
+  "view": "person-person-update-panel"
+});
 
 panelRegistry.registerType("project.Project", {
-     "edit" : "project-project-update-panel" ,"view" : "project-project-update-panel"
- });
+  "edit": "project-project-update-panel",
+  "view": "project-project-update-panel"
+});
 
 panelRegistry.registerType("task.Task", {
-     "edit" : "task-task-update-panel" ,"view" : "task-task-update-panel"
- });
+  "edit": "task-task-update-panel",
+  "view": "task-task-update-panel"
+});
 
 panelRegistry.registerType("tree.Tree", {
-     "edit" : "tree-tree-update-panel" ,"view" : "tree-tree-update-panel"
- });
+  "edit": "tree-tree-update-panel",
+  "view": "tree-tree-update-panel"
+});
 
