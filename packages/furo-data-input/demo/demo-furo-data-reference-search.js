e6f3beee98721f4648a656fb9e1aedc40a372f59
Zou Fan <fan.zou@gmail.com>
2019-09-13 07:44:37 +0200

    chore: remove necessary code

--- packages/furo-data-input/demo/demo-furo-data-reference-search.js
+++ packages/furo-data-input/demo/demo-furo-data-reference-search.js
@@ -50,29 +50,11 @@ class DemoFuroDataReferenceSearch extends FBP(LitElement) {
         <h2>Demo furo-data-reference-search</h2>
         <p>Bind the field from furo-data-object with <strong>ƒ-bind-data="--entityReady(*.fields.fieldname)"</strong>.
           The labels, hints, defaults are comming from the furo-data-object specs.</p>
-        <div flex>
-             <furo-data-reference-search  label="test"  flex ƒ-bind-data="--entity(*.owner)" min-term-length="2" @-search="--term" ƒ-collection-in="--refCol"></furo-data-reference-search>
-             <furo-data-reference-search hint="please search the owner with name" label="task owner search" flex ƒ-bind-data="--entity(*.owner)" min-term-length="2" @-search="--term" ƒ-collection-in="--refCol"></furo-data-reference-search>
-              
-            <produce-qp-data @-data="--qp" qp={"tsk":1}></produce-qp-data>
-
-            <furo-data-object type="task.Task" @-object-ready="--entity"
-                              ƒ-inject-raw="--response(*.data)"></furo-data-object>
-            <furo-deep-link service="TaskService" @-hts-out="--hts" ƒ-qp-in="--qp"></furo-deep-link>
-            <furo-deep-link service="PersonService" @-hts-out="--Phts" ƒ-qp-in="--qp"></furo-deep-link>
-           
-            <furo-entity-agent service="TaskService"
-                               ƒ-hts-in="--hts"
-                               ƒ-load="--hts"
-                               ƒ-bind-request-data="--entity"
-                               @-response="--response">
-            </furo-entity-agent>
-              
-            <furo-collection-agent service="PersonService" ƒ-search="--term" ƒ-hts-in="--entity(*.owner.link.value)" @-response="--refCol"></furo-collection-agent>
-              
-        
-
-        </div>
+          <div flex>
+               <furo-data-reference-search hint="please search the owner with name" label="task owner search" flex ƒ-bind-data="--entity(*.owner)" min-term-length="2" @-search="--term" ƒ-collection-in="--refCol"></furo-data-reference-search>
+              <furo-data-object type="task.Task" @-object-ready="--entity"></furo-data-object>
+              <furo-collection-agent service="PersonService" ƒ-search="--term" ƒ-hts-in="--entity(*.owner.link.value)" @-response="--refCol"></furo-collection-agent>
+          </div>
       </furo-vertical-flex>
     `;
   }
