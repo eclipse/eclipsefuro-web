cb3937bc540550ebb31889f2647389fd7d40c9d4
veith <veithz@gmail.com>
2019-09-17 09:55:15 +0200

    feat: property field init

--- packages/furo-data-input/demo/demo-furo-data-property.js
+++ packages/furo-data-input/demo/demo-furo-data-property.js
@@ -44,29 +44,24 @@ class DemoFuroDataProperty extends FBP(LitElement) {
     return html`
       <furo-vertical-flex>
         <div>
-          <h2>Demo ...</h2>
-          <p>Describe your demo</p>
+          <h2>Demo furo-data-property</h2>
+          <p>Bind your fields as usual.</p>
         </div>
         <furo-demo-snippet flex>
           <template>
             <furo-vertical-scroller>
-              <furo-data-property ƒ-bind-data="--entity(*.single_type_property)"></furo-data-property>
-              <furo-data-property ƒ-bind-data="--entity(*.single_type_property)"></furo-data-property>
+              <furo-form-layouter two>
+                <!-- single Property -->
+                <furo-data-property ƒ-bind-data="--entity(*.single_type_property)"></furo-data-property>
+                <!-- repeated Property -->
+                <furo-data-property ƒ-bind-data="--entity(*.type_property)"></furo-data-property>
+                <!-- single Property -->
+                <furo-data-property ƒ-bind-data="--entity(*.single_type_property)"></furo-data-property>
+              </furo-form-layouter>
               
-              <furo-horizontal-flex>
-                <div>
-              <template is="flow-repeat" ƒ-inject-items="--dd(*.type_property.repeats)" identity-path="id.value">
-                <furo-data-property ƒ-bind-data="--item"></furo-data-property>
-              </template>
-                </div>
-                <div>
-              <template is="flow-repeat" ƒ-inject-items="--dd(*.type_property.repeats)" identity-path="id.value">
-                <furo-data-property ƒ-bind-data="--item"></furo-data-property>
-              </template>
-                </div>
-              </furo-horizontal-flex>
+
               <produce-qp-data auto @-data="--qp" qp={"exp":1}></produce-qp-data>
-              <furo-data-object type="experiment.Experiment" @-object-ready="--entity" @-data-injected="--dd"
+              <furo-data-object type="experiment.Experiment" @-object-ready="--entity"
                                 ƒ-inject-raw="--response(*.data)"></furo-data-object>
               <furo-deep-link service="ExperimentService" @-hts-out="--hts" ƒ-qp-in="--qp"></furo-deep-link>
               <furo-entity-agent service="ExperimentService"
--- packages/furo-data-input/furo-data-property.js
+++ packages/furo-data-input/furo-data-property.js
@@ -2,12 +2,60 @@ import {LitElement, html, css} from 'lit-element';
 import {Theme} from "@furo/framework/theme"
 import {FBP} from "@furo/fbp";
 import {NodeEvent} from "@furo/data/lib/EventTreeNode";
+import {RepeaterNode} from "@furo/data/lib/RepeaterNode";
 
 /**
  * `furo-data-property`
- * todo Describe your element
+ *  Field for type furo.Property. It works with repeated types an nonrepeating type. Supported
  *
- * @summary todo shortdescription
+ *  ```html
+ *  <!-- single Property -->
+ *  <furo-data-property ƒ-bind-data="--entity(*.single_type_property)"></furo-data-property>
+ *  <!-- repeated Property -->
+ *  <furo-data-property ƒ-bind-data="--entity(*.type_property)"></furo-data-property>
+ *
+ *  ```
+ *
+ *  ## Example data for the data-object looks like this
+ *
+ *  ```json
+ *  "single_type_property": {
+ *    "data": {
+ *      "@type": "google.type.Date",
+ *      "day": 8,
+ *      "month":  11,
+ *      "year": 2022
+ *    },
+ *    "display_name": "a date",
+ *    "id": "date",
+ *    "code": "date",
+ *    "meta": {
+ *      "fields": {
+ *        "data": {
+ *          "meta": {
+ *            "label": "Additional fields",
+ *            "hint": "this is data"
+ *          },
+ *          "constraints": {
+ *            "min": {
+ *              "value": "2019-09-09",
+ *              "message": "to small"
+ *            }
+ *          }
+ *        }
+ *      }
+ *    }
+ *  }
+ *  ```
+ *
+ *  The current type mappings are:
+ *
+ *  -  "google.type.Date": "furo-data-date-input",
+ "  -  furo.StringProperty": "furo-data-text-input",
+ "  -  furo.NumberProperty": "furo-data-number-input"
+
+ *
+ * @summary property input
  * @customElement
  * @demo demo-furo-data-property
  * @appliesMixin FBP
@@ -19,83 +67,72 @@ class FuroDataProperty extends FBP(LitElement) {
     this.typemap = {
       "google.type.Date": "furo-data-date-input",
       "furo.StringProperty": "furo-data-text-input",
-      "string": "furo-data-text-input"
+      "furo.NumberProperty": "furo-data-number-input"
     };
   }
 
   bindData(propertyField) {
+
     this.field = propertyField;
-    this.field.data.addEventListener('branch-value-changed', (d) => {
-      this.type = this.field.data["@type"];
-      this._createPropComponent(propertyField);
-    }, {once: true});
+
+    if (propertyField instanceof RepeaterNode) {
+
+      // add flow repeat to parent and inject on repeated changes
+      // repeated
+      let r = document.createElement("flow-repeat");
+      r.setAttribute("identity-path", "id.value");
+      r.innerHTML = "<template><furo-data-property ƒ-bind-data=\"--item\"></furo-data-property></template>";
+      let repeater = this.parentNode.insertBefore(r, this);
 
 
-    // data already in data-object
-    if (this.field.data["@type"]) {
-      this.type = this.field.data["@type"];
-      this._createPropComponent(propertyField);
+      this.field.addEventListener('this-repeated-field-changed', (data) => {
+        repeater.injectItems(this.field.repeats);
+      });
+
+    } else {
+      this.field.data.addEventListener('branch-value-changed', (d) => {
+
+        this._createPropComponent(propertyField);
+
+      }, {once: true});
+
+
+      // data already in data-object
+      if (this.field.data["@type"]) {
+        this._createPropComponent(propertyField);
+      }
     }
+
   }
 
   _createPropComponent(propertyField) {
     if (!this._property_created) {
-      let e = document.createElement(this.typemap[this.type]);
-      switch (this.type) {
-        case "google.type.Date":
-          e.bindData(propertyField.data);
-          break;
+      let e = document.createElement(this.typemap[propertyField.data["@type"]]);
+      switch (propertyField.data["@type"]) {
+          // the input elements for string and number are just working with scalar values
         case "furo.StringProperty":
+        case "furo.NumberProperty":
           e.bindData(propertyField.data.data);
           break;
         default:
           e.bindData(propertyField.data);
       }
 
-      this.shadowRoot.appendChild(e);
+      this.parentNode.insertBefore(e, this);
       propertyField.data.dispatchNodeEvent(new NodeEvent('this-metas-changed', propertyField.data, false));
       this._property_created = true;
     }
   }
 
-  /**
-   * @private
-   * @return {Object}
-   */
-  static get properties() {
-    return {
-      /**
-       * Description
-       */
-      myBool: {type: Boolean}
-    };
-  }
-
-  /**
-   * flow is ready lifecycle method
-   */
-  _FBPReady() {
-    super._FBPReady();
-    //this._FBPTraceWires()
-  }
-
-  /**
-   * Themable Styles
-   * @private
-   * @return {CSSResult}
-   */
   static get styles() {
     // language=CSS
     return Theme.getThemeForComponent(this.name) || css`
         :host {
-            display: block;
-        }
-
-        :host([hidden]) {
             display: none;
         }
     `
   }
+
 }
 
 window.customElements.define('furo-data-property', FuroDataProperty);
