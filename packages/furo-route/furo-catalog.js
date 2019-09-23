fa21b79d01fde3b5ceaf160f48a0f07ed2b701cf
veith <veithz@gmail.com>
2019-09-23 21:11:42 +0200

    chore: ordering for documentation

--- packages/furo-route/furo-catalog.js
+++ packages/furo-route/furo-catalog.js
@@ -1,6 +1,6 @@
-import "./furo-pages"
-import "./furo-location"
 import "./furo-app-flow"
 import "./furo-app-flow-router"
+import "./furo-location"
+import "./furo-pages"
 import "./furo-panel-coordinator"
 import "./furo-qp-changer"
