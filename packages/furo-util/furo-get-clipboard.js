06a2e7d1ed4e8e518d0853f81c87baf7782f241b
veith <veithz@gmail.com>
2019-09-28 17:22:47 +0200

    feat: furo-put-clipboard

--- packages/furo-util/furo-get-clipboard.js
+++ packages/furo-util/furo-get-clipboard.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import {LitElement, html, css} from 'lit-element';
 import {FBP} from "@furo/fbp";
 
 /**
@@ -18,20 +18,40 @@ import {FBP} from "@furo/fbp";
  */
 class FuroGetClipboard extends FBP(LitElement) {
 
-    trigger(){
-      navigator.clipboard.readText().then(
-          clipText => {
-            /**
-            * @event content
-            * Fired when clipboard content is received
-            * detail payload: {*} content of the clipboard
-            */
-            let customEvent = new Event('content', {composed:true, bubbles: true});
+  trigger() {
+    navigator.clipboard.readText().then(
+        clipText => {
+          /**
+           * @event content
+           * Fired when clipboard content is received
+           * detail payload: {*} content of the clipboard
+           */
+          let customEvent = new Event('content', {composed: true, bubbles: true});
+          if (this.json) {
+            customEvent.detail = JSON.parse(clipText);
+          } else {
             customEvent.detail = clipText;
-            this.dispatchEvent(customEvent)
-          })
-    }
+          }
+
+          this.dispatchEvent(customEvent)
+        })
+  }
+
+
+  /**
+   * @private
+   * @return {Object}
+   */
+  static get properties() {
+    return {
+      /**
+       * Convert clipboard content to json
+       */
+      json: {type: Boolean}
+    };
+  }
 
 }
 
 window.customElements.define('furo-get-clipboard', FuroGetClipboard);
+
