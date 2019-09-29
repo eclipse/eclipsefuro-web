e35af479035c436c75355f846f16f4455781b981
veith <veithz@gmail.com>
2019-09-29 15:08:39 +0200

    feat: furo-fetch-json component added

---
+++ packages/furo-util/furo-fetch-json.js
@@ -0,0 +1,63 @@
+import {LitElement, html, css} from 'lit-element';
+import {Theme} from "@furo/framework/theme"
+import {FBP} from "@furo/fbp";
+
+/**
+ * `furo-fetch-json`
+ *  Fetches and parses json data from a source.
+ *
+ *  ```html
+ *  <furo-fetch-json src="/example.json" ƒ-fetch="--FBPready" @-data="--contentReceived"></furo-fetch-json>
+ *  ```
+ *
+ * @summary fetch json data
+ * @customElement
+ * @appliesMixin FBP
+ */
+class FuroFetchJson extends FBP(LitElement) {
+
+  /**
+   * @private
+   * @return {Object}
+   */
+  static get properties() {
+    return {
+      /**
+       * the url you want to fetch
+       */
+      src: {type: String}
+    };
+  }
+
+  /**
+   * fetch and parse the data from specified `src`.
+   *
+   * Use fetch-src if you want to pass the source url
+   */
+  fetch() {
+    if (this.src) {
+      fetch(this.src).then(res => res.json()).then(data => {
+        /**
+         * @event data
+         * Fired when data received and json parsed
+         * detail payload: {Object} json data
+         */
+        let customEvent = new Event('data', {composed: true, bubbles: true});
+        customEvent.detail = data;
+        this.dispatchEvent(customEvent);
+      });
+    }
+  }
+
+  /**
+   * fetch json data from source
+   * @param source
+   */
+  fetchSrc(source) {
+    this.src = source;
+    this.fetch();
+  }
+
+}
+
+window.customElements.define('furo-fetch-json', FuroFetchJson);
