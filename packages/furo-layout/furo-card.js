9e87dbf7de1a13bbd9cbe5997181d2a2b6fddf91
maltenorstroem <malte.norstroem@gmail.com>
2019-10-21 15:45:12 +0200

    feat: generated update.panel.js with progress indicator and hateoas check on action bar items

--- packages/furo-layout/furo-card.js
+++ packages/furo-layout/furo-card.js
@@ -15,144 +15,145 @@ import {FBP} from "@furo/fbp";
  */
 class FuroCard extends FBP(LitElement) {
 
-  /**
-   * @private
-   * @return {Object}
-   */
-  static get properties() {
-    return {
-      /**
-       * Main title
-       */
-      title: {type: String},
-      secondaryText: {type: String, attribute: "secondary-text"},
-      hasaction: {type: Boolean, reflect: true},
-      hasmedia: {type: Boolean, reflect: true}
-    };
-  }
+    /**
+     * @private
+     * @return {Object}
+     */
+    static get properties() {
+        return {
+            /**
+             * Main title
+             */
+            title: {type: String},
+            secondaryText: {type: String, attribute: "secondary-text"},
+            hasaction: {type: Boolean, reflect: true},
+            hasmedia: {type: Boolean, reflect: true}
+        };
+    }
 
 
-  constructor() {
-    super();
-    this.hasaction = this.querySelectorAll('*[slot="action"]').length > 0;
-    this.hasmedia = this.querySelectorAll('*[slot="media"]').length > 0;
-  }
+    constructor() {
+        super();
+        this.hasaction = this.querySelectorAll('*[slot="action"]').length > 0;
+        this.hasmedia = this.querySelectorAll('*[slot="media"]').length > 0;
+    }
 
-  /**
-   *
-   * @private
-   * @return {CSSResult}
-   */
-  static get styles() {
-    // language=CSS
-    return Theme.getThemeForComponent(this.name) || css`
-        :host {
-            display: block;
-            box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
-            0 1px 5px 0 rgba(0, 0, 0, 0.12),
-            0 3px 1px -2px rgba(0, 0, 0, 0.2);
-            --furo-button-padding: var(--spacing-xs, 8px);
-            background: var(--furo-card-background, var(--surface, white));
-            padding-bottom: var(--furo-card-padding, var(--spacing-xs, 8px));
-            margin: var(--furo-card-margin, 0);
-            box-sizing: border-box;
-            position: relative;
-            border-radius: 4px;
-            font-size: 14px;
-            letter-spacing: 0.1px;
-        }
-
-
-        :host([hidden]) {
-            display: none;
-        }
-
-        :host([hasaction]) .content {
-            padding-bottom: 44px;
-        }
+    /**
+     *
+     * @private
+     * @return {CSSResult}
+     */
+    static get styles() {
+        // language=CSS
+        return Theme.getThemeForComponent(this.name) || css`
+            :host {
+                display: block;
+                box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+                0 1px 5px 0 rgba(0, 0, 0, 0.12),
+                0 3px 1px -2px rgba(0, 0, 0, 0.2);
+                --furo-button-padding: var(--spacing-xs, 8px);
+                background: var(--furo-card-background, var(--surface, white));
+                padding-bottom: var(--furo-card-padding, var(--spacing-xs, 8px));
+                margin: var(--furo-card-margin, 0);
+                box-sizing: border-box;
+                position: relative;
+                border-radius: 4px;
+                font-size: 14px;
+                letter-spacing: 0.1px;
+            }
 
 
-        /** no padding-top on .content if title is set **/
-        :host([title]) .content {
-            padding-top: 0;
-        }
-        
-        /** set padding-top on .content if media is present **/
-        :host([title][hasmedia]) .content {
-            padding-top: var(--furo-card-padding, var(--spacing-s, 16px));
-        }
-        
-        .content {
-            padding: var(--furo-card-padding, var(--spacing-s, 16px));
-        }
+            :host([hidden]) {
+                display: none;
+            }
 
-        .content ::slotted(h1){
-        
-            font-size: 24px;
-            line-height: 24px;
-            letter-spacing: 0;
-            margin: 0;
-            font-weight: normal;
-            margin-bottom: 4px;
-        
-        }
-        .action {
-            position: absolute;
-            bottom: var(--furo-card-padding, var(--spacing-xs, 8px));
-            left: var(--furo-card-padding, var(--spacing-xs, 8px));
-            right: var(--furo-card-padding, var(--spacing-xs, 8px));
-
-        }
+            :host([hasaction]) .content {
+                padding-bottom: 44px;
+            }
 
 
-        :host([title]) .head {
-            display: block;
-        }
+            /** no padding-top on .content if title is set **/
+            :host([title]) .content {
+                padding-top: 0;
+            }
 
-        .head {
-            display: none;
-            padding: var(--spacing-s, 16px);
-        }
+            /** set padding-top on .content if media is present **/
+            :host([title][hasmedia]) .content {
+                padding-top: var(--furo-card-padding, var(--spacing-s, 16px));
+            }
 
-        .head span {
-            color: var(--secondary-color, var(--on-primary-light,#777777));
-            line-height: 22px;
-        }
+            .content {
+                padding: var(--furo-card-padding, var(--spacing-s, 16px));
+            }
 
-        h1 {
-            font-size: 24px;
-            line-height: 24px;
-            letter-spacing: 0;
-            margin: 0;
-            font-weight: normal;
-            margin-bottom: 4px;
-        }
+            .content ::slotted(h1) {
 
-        :host([title]) .media ::slotted(*) {
-            border-radius: 0;
-        }
+                font-size: 24px;
+                line-height: 24px;
+                letter-spacing: 0;
+                margin: 0;
+                font-weight: normal;
+                margin-bottom: 4px;
 
-        .media ::slotted(*) {
-            border-top-right-radius: 4px;
-            border-top-left-radius: 4px;
-            width: 100%;
-        }
+            }
 
-    `
-  }
+            .action {
+                position: absolute;
+                bottom: var(--furo-card-padding, var(--spacing-xs, 8px));
+                left: var(--furo-card-padding, var(--spacing-xs, 8px));
+                right: var(--furo-card-padding, var(--spacing-xs, 8px));
+
+            }
 
 
-  /**
-   * @private
-   * @returns {TemplateResult}
-   */
-  render() {
-    // language=HTML
-    return html`
-        <div class="head">
-          <h1>${this.title}</h1>
-        <span>${this.secondaryText}</span>  
-        </div>
+            :host([title]) .head {
+                display: block;
+            }
+
+            .head {
+                display: none;
+                padding: var(--spacing-s, 16px);
+            }
+
+            .head span {
+                color: var(--secondary-color, var(--on-primary-light, #777777));
+                line-height: 22px;
+            }
+
+            h1 {
+                font-size: 24px;
+                line-height: 24px;
+                letter-spacing: 0;
+                margin: 0;
+                font-weight: normal;
+                margin-bottom: 4px;
+            }
+
+            :host([title]) .media ::slotted(*) {
+                border-radius: 0;
+            }
+
+            .media ::slotted(*) {
+                border-top-right-radius: 4px;
+                border-top-left-radius: 4px;
+                width: 100%;
+            }
+
+        `
+    }
+
+    /**
+     * @private
+     * @returns {TemplateResult}
+     */
+    render() {
+        // language=HTML
+        return html`
+      <furo-loading-indicator-bar ƒ-start="--activityStarted" ƒ-stop="--activityStopped"></furo-loading-indicator-bar>
+      <div class="head">
+        <h1>${this.title}</h1>
+      <span>${this.secondaryText}</span>  
+      </div>
       <div class="media">
       <slot name="media"></slot>
       </div>
@@ -163,7 +164,23 @@ class FuroCard extends FBP(LitElement) {
         <slot name="action"></slot>
       </div>
     `;
-  }
+    }
+
+    /**
+     * You can show a progress indicator while you have pending requests or work
+     * Shows furo-loading-indicator-bar
+     */
+    startActivity() {
+        this._FBPTriggerWire('--activityStarted');
+    }
+
+    /**
+     * Stop loading indicator
+     * Hides furo-loading-indicator-bar
+     */
+    stopActivity() {
+        this._FBPTriggerWire('--activityStopped');
+    }
 }
 
 window.customElements.define('furo-card', FuroCard);
