3b636792eb6a3562550f2a25beeeddecdb715fca
veith <veithz@gmail.com>
2019-12-03 19:46:58 +0100

    feat: experimental tooltip

---
+++ packages/furo-app/furo-tooltip-display.js
@@ -0,0 +1,132 @@
+import {LitElement, html, css} from 'lit-element';
+import {Theme} from "@furo/framework/theme"
+import {FBP} from "@furo/fbp";
+
+/**
+ * `furo-tooltip`
+ * todo Describe your element
+ *
+ * @summary todo shortdescription
+ * @customElement
+ * @demo demo-furo-tooltip
+ * @appliesMixin FBP
+ */
+class FuroTooltipDisplay extends FBP(LitElement) {
+
+  constructor() {
+    super();
+
+    // return **this** to component which want to connect
+    window.addEventListener("hide-tooltip-requested", (e) => {
+      if (this.show) {
+        clearTimeout(this.to)
+        this.show = false;
+      }
+    });
+    window.addEventListener("show-tooltip-requested", (e) => {
+
+
+      let cr = e.detail.cr;
+      let x = cr.left + cr.width / 2;
+      let y = cr.bottom + 16;
+
+
+      this.style.top = y + "px";
+      this.start = true;
+      this.label = e.detail.label;
+      clearTimeout(this.to);
+
+      setTimeout(() => {
+        let mycr = this.getBoundingClientRect();
+        let max = window.innerWidth - 8;
+        this.style.left = Math.min(max - mycr.width , Math.max(8,x - mycr.width / 2)) + "px";
+
+        this.show = true;
+        this.to = setTimeout(() => {
+          // hide if shown
+          if (this.show) {
+            this.show = false;
+          }
+        }, e.detail.duration)
+      }, 0)
+
+
+    })
+
+  }
+
+  /**
+   * @private
+   * @return {Object}
+   */
+  static get properties() {
+    return {
+      /**
+       * Description
+       */
+      start: {type: Boolean, reflect: true},
+      show: {type: Boolean, reflect: true}
+    };
+  }
+
+  /**
+   * flow is ready lifecycle method
+   */
+  _FBPReady() {
+    super._FBPReady();
+    //this._FBPTraceWires()
+
+  }
+
+  /**
+   * Themable Styles
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return Theme.getThemeForComponent(this.name) || css`
+      :host {
+        position: absolute;
+        display: block;
+        transition: opacity 300ms;
+        opacity: 0;
+        background-color: #6d6d6d;
+        color: white;
+        height: 24px;
+        padding: 0 8px;
+        border-radius: 4px;
+        font-size: 12px;
+        line-height: 24px;
+        z-index: 10;
+        white-space: nowrap;
+      }
+
+      :host([start]) {
+        display: block;
+        opacity: 0;
+      }
+
+      :host([show]) {
+        opacity: 1;
+        display: block;
+
+      }
+    `
+  }
+
+
+  /**
+   * @private
+   * @returns {TemplateResult}
+   * @private
+   */
+  render() {
+    // language=HTML
+    return html`
+      ${this.label}
+    `;
+  }
+}
+
+window.customElements.define('furo-tooltip-display', FuroTooltipDisplay);
---
+++ packages/furo-app/furo-tooltip.js
@@ -0,0 +1,108 @@
+import { LitElement, html, css } from 'lit-element';
+import {Theme} from "@furo/framework/theme"
+import {FBP} from "@furo/fbp";
+
+/**
+ * `furo-tooltip`
+ * EXPERIMENTAL, API MAY CHANGE
+ *
+ * @summary
+ * @customElement
+ * @demo demo-furo-tooltip
+ * @appliesMixin FBP
+ */
+class FuroTooltip extends FBP(LitElement) {
+
+constructor(){
+  super();
+  /**
+   * display duration in ms
+   * @type {number}
+   */
+  this.duration = 1500;
+  this.parentNode.addEventListener("mouseover",(e)=>{
+    // client rectangle
+    this.cr = this.parentNode.getBoundingClientRect();
+
+    /**
+    * @event show-tooltip-requested
+    * Fired when mouseover on component
+    * detail payload:
+    */
+    let customEvent = new Event('show-tooltip-requested', {composed:true, bubbles: true});
+    customEvent.detail = this;
+    this.dispatchEvent(customEvent)
+  });
+
+
+   this.parentNode.addEventListener("mouseout",(e)=>{
+    /**
+    * @event show-tooltip-requested
+    * Fired when mouseover on component
+    * detail payload:
+    */
+    let customEvent = new Event('hide-tooltip-requested', {composed:true, bubbles: true});
+    customEvent.detail = this;
+    this.dispatchEvent(customEvent)
+  });
+
+
+
+
+}
+    /**
+     * @private
+     * @return {Object}
+     */
+    static get properties() {
+        return {
+            /**
+             * Display duration in ms
+             */
+            duration: {type: Number},
+            label: {type: String}
+        };
+    }
+
+  /**
+  * flow is ready lifecycle method
+  */
+  _FBPReady(){
+    super._FBPReady();
+    //this._FBPTraceWires()
+
+  }
+
+  /**
+   * Themable Styles
+   * @private
+   * @return {CSSResult}
+   */
+  static get styles() {
+    // language=CSS
+    return Theme.getThemeForComponent(this.name) || css`
+        :host {
+            display: block;
+        }
+
+        :host([hidden]) {
+            display: none;
+        }
+    `
+  }
+
+
+  /**
+   * @private
+   * @returns {TemplateResult}
+   * @private
+   */
+  render() {
+    // language=HTML
+    return html`
+      <p>Hej, welcome</p>
+    `;
+  }
+}
+
+window.customElements.define('furo-tooltip', FuroTooltip);
