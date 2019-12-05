67e8e2c76bc4234912c1018d1fdf3fd75b5c527a
maltenorstroem <malte.norstroem@gmail.com>
2019-12-05 16:41:28 +0100

    feat: new panel component to simply wrap elements with a predefined container

--- packages/furo-layout/demo/demos.js
+++ packages/furo-layout/demo/demos.js
@@ -1,4 +1,5 @@
-import "./demo-furo-horizontal-flex"
-import "./demo-furo-vertical-flex"
-import "./demo-furo-vertical-scroller"
-import "./demo-furo-split-view"
+import "./demo-furo-horizontal-flex.js";
+import "./demo-furo-vertical-flex.js";
+import "./demo-furo-vertical-scroller.js";
+import "./demo-furo-split-view.js";
+import "./demo-furo-panel.js";
