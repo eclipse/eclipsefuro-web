77715f531e570fc8fd1cd881b2fae0a3fcf290fb
Zou Fan <fan.zou@gmail.com>
2019-12-09 16:32:17 +0100

    chore: tests for panel coordinator

--- packages/furo-data-input/furo-data-color-input.js
+++ packages/furo-data-input/furo-data-color-input.js
@@ -100,14 +100,14 @@ class FuroDataColorInput extends FBP(LitElement) {
    * Sets the field to readonly
    */
   disable() {
-    this._readonly = true;
+    this.disabled = true;
   }
 
   /**
    * Makes the field writable.
    */
   enable() {
-    this._readonly = false;
+    this.disabled = false;
   }
 
 
