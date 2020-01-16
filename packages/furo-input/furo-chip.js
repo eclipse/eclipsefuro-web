eb4009dcc7e139411375c6f0cce548d2955e5018
Zou Fan <fan.zou@gmail.com>
2020-01-16 09:09:44 +0100

    feat: furo-chip basis component

--- packages/furo-input/furo-chip.js
+++ packages/furo-input/furo-chip.js
@@ -3,16 +3,22 @@ import {Theme} from "@furo/framework/theme"
 import {FBP} from "@furo/fbp";
 import "@furo/layout/furo-ripple";
 /**
- * `furo-choice-chip`
+ * `furo-chip`
  *
- * Checkbox input element which uses a native `<input type="checkbox">` tag.
+ * Chips are compact elements that allow users to enter information, select a choice, filter content, or trigger an action.
  *
- * Checkboxes allow the user to to make a binary choice, i.e. a choice between one of two possible mutually exclusive options.
  *
  * ### Sample
  *  <furo-demo-snippet>
  *   <template>
- *    <furo-choice-chip></furo-choice-chip>
+ *    <furo-chip  selected condensed text="selected"  ></furo-chip>
+ *    <furo-chip  condensed text="selected"  ></furo-chip>
+ *    <br/>
+ *    <br/>
+ *    <br/>
+ *    <furo-chip selected outlined  leading-icon="schedule" trailing-icon="close" text="hallo world" ></furo-chip>
+ *    <furo-chip  outlined  leading-icon="schedule" trailing-icon="close" text="hallo world" ></furo-chip>
+ *
  *   </template>
  *  </furo-demo-snippet>
  *
@@ -21,11 +27,25 @@ import "@furo/layout/furo-ripple";
  *
  * Custom property | Description | Default  | Fallback
  * ----------------|-------------|----------|----------
- * `--choise-chip-unselected--bg-color` | background color of the unselected chip | `--background` | hex: #eeeeee
+ * `--input-chip-unselected--bg-color` | background color of the unselected chip | `--background` | hex: #eeeeee
+ * `--input-chip-outlined-unselected--bg-color` | background color of the unselected outlined chip |  | hex: #ffffff
+ * `--input-chip-selected-bg-color` | background color of the selected chip | `--primary` | hex: #4caf4f
+ * `--input-chip-selected-color` |  color of the selected chip | `--on-primary` | hex: #ffffff
+ * `--input-chip-outlined-selected-bg-color-rgb` | rgba color of the background of the selected chip |  `--primary-rgb` with `--state-active` opacity | rgba: 76, 175, 80, 0.10
+ * `--input-chip-outlined-selected-text-color` |  color of the text of the outlined selected chip |  `--primary`  | hex: #4caf50
+ * `--input-chip-outlined-selected-border-color` | border color of the outlined selected chip |  `--on-background`  | hex: #000000
+ * `--input-chip-outlined-selected-hover-bg-color-rgb` | rgba color of the background of the outlined selected chip by hovering|  `--on-background-rgb` with opacity `--state-hover` | rgba:  33, 33, 33, 0.04
+ * `--input-chip-unselected-hover-bg-color-rgb` | rgba color of the background of the unselected chip by hovering|  `--on-background-rgb` with opacity `--state-hover` | rgba:  33, 33, 33, 0.04
+ * `--input-chip-unselected-active-bg-color-rgb` | rgba color of the background of the unselected chip by clicking|  `--on-background-rgb` with opacity `--state-active` | rgba:  33, 33, 33, 0.10
+ * `--input-chip-unselected-focus-bg-color-rgb` | rgba color of the background of the unselected chip by focusing|  `--on-background-rgb` with opacity `--state-focus` | rgba:  33, 33, 33, 0.12
+ * `--input-chip-selected-active-bg-color-rgb` | rgba color of the background of the selected chip by clicking|  `--primary-rgb` with opacity `--state-active` | rgba:  76, 175, 80, 0.10
+ * `--input-chip-selected-focus-bg-color-rgb` | rgba color of the background of the selected chip by focusing|  `--primary-rgb` with opacity `--state-focus` | rgba:  76, 175, 80, 0.12
+ * `--input-chip-disabled-selected-bg-color-rgb` | rgba color of the background of the selected disabled chip|  `--on-background-rgb` with opacity `--state-disabled` | rgba:  33, 33, 33, 0.38
+ * `--input-chip-outlined-disabled-selected-bg-color-rgb` | rgba color of the background of the outlined selected disabled chip|  `--on-background-rgb` with opacity `--state-disabled` | rgba:  33, 33, 33, 0.38
  *
  * @summary chips
  * @customElement
- * @demo demo-furo-choice-chip Basic demo
+ * @demo demo-furo-chip Basic demo
  * @appliesMixin FBP
  */
 class FuroChip extends FBP(LitElement) {
@@ -41,8 +61,8 @@ class FuroChip extends FBP(LitElement) {
 
       let input = e.composedPath()[0];
 
-      this.selected = input.selected;
-      this.value = input.selected;
+      this.selected = input.checked;
+      this.value = input.checked;
     });
 
     this.addEventListener("click", (e)=>{
@@ -61,39 +81,39 @@ class FuroChip extends FBP(LitElement) {
   }
 
   /**
-   * Sets the focus on the checkbox.
+   * Sets the focus on the chip.
    */
   focus() {
     this._FBPTriggerWire("--focus");
   }
 
   /**
-   * check the checkbox
+   * select the chip
    */
-  check() {
+  select() {
     this.selected = true;
     this.value = true;
   }
 
   /**
-   * uncheck the checkbox
+   * deselect the chip
    */
-  uncheck() {
+  deselect() {
     this.selected = false;
     this.value = false;
   }
 
   /**
-   * toggle the checkbox
+   * toggle the chip
    */
   toggle() {
     this.shadowRoot.getElementById("input").click();
   }
 
   /**
-   * Sets the value for the checkbox
-   * The value of checkbox with true (selected) or false (unselected). Changes will be notified with the `@-value-changed` event
-   * This is different from the native attribute `value` of the input checkbox
+   * Sets the value for the chip
+   * The value of chip with true (selected) or false (unselected). Changes will be notified with the `@-value-changed` event
+   * This is different from the native attribute `value` of the input chip
    * @param {boolean} v
    */
   setValue(v) {
@@ -118,7 +138,7 @@ class FuroChip extends FBP(LitElement) {
 
       /**
        * @event selected
-       * Fired when the checkbox is selected
+       * Fired when the chip is selected
        * detail payload: {String} the text value
        */
       let customEvent = new Event('selected', {composed: true, bubbles: true});
@@ -128,7 +148,7 @@ class FuroChip extends FBP(LitElement) {
     else {
       /**
        * @event unselected
-       * Fired when the checkbox is unselected
+       * Fired when the chip is unselected
        * detail payload: {String} the text value
        */
       let customEvent = new Event('unselected', {composed: true, bubbles: true});
@@ -145,8 +165,8 @@ class FuroChip extends FBP(LitElement) {
     return {
 
       /**
-       * The value of checkbox with true (selected) or false (unselected). Changes will be notified with the `@-value-changed` event
-       * This is different from the native attribute `value` of the input checkbox
+       * The value of chip with true (selected) or false (unselected). Changes will be notified with the `@-value-changed` event
+       * This is different from the native attribute `value` of the input chip
        */
       value: {
         type: Boolean
@@ -167,7 +187,7 @@ class FuroChip extends FBP(LitElement) {
       },
 
       /**
-       * A Boolean attribute which, if present, means this checkbox is selected.
+       * A Boolean attribute which, if present, means this chip is selected.
        */
       selected: {
         type: Boolean, reflect: true
@@ -180,19 +200,30 @@ class FuroChip extends FBP(LitElement) {
         type: Boolean, reflect: true
       },
 
+      /**
+       * Text in chip.
+       */
       text: {
         type: String
       },
-
+      /**
+       * A Boolean attribute which, if present, means the chip is outlined.
+       */
       outlined: {
         type: Boolean
       },
 
+      /**
+       * the leading icon of the chip.
+       */
       leadingIcon: {
         type: String,
         attribute: "leading-icon"
       },
 
+      /**
+       * the trailing icon of the chip.
+       */
       trailingIcon: {
         type: String,
         attribute: "trailing-icon"
@@ -226,12 +257,12 @@ class FuroChip extends FBP(LitElement) {
                 user-select: none;
                 height: 40px;
                 border-radius: 20px;
-                background-color: var(--choise-chip-unselected--bg-color, var(--background, #eeeeee));
+                background-color: var(--input-chip-unselected--bg-color, var(--background, #eeeeee));
                 padding: 8px;
                 display: inline-block;
 
             }
-            /* input checkbox*/
+            /* input chip*/
             :host input {
                 z-index: -1;
                 position: absolute;
@@ -240,73 +271,73 @@ class FuroChip extends FBP(LitElement) {
             }
 
             :host([outlined]){
-                background-color: var(--choise-chip-outlined-unselected--bg-color, #ffffff);
+                background-color: var(--input-chip-outlined-unselected--bg-color, #ffffff);
                 border: solid 1px;
             }
 
             /* selected choice chip  */
             :host([selected]){
-                background-color: var(--choise-chip-selected-bg-color, var(--primary, #4caf4f));
-                color: var(--choise-chip-selected-color, var(--on-primary, #ffffff));
+                background-color: var(--input-chip-selected-bg-color, var(--primary, #4caf4f));
+                color: var(--input-chip-selected-color, var(--on-primary, #ffffff));
             }
 
             :host([outlined][selected]){
-                background-color: rgba( var(--choise-chip-outlined-selected-bg-color-rgb, var(--primary-rgb, 76, 175, 80)), var(--state-active, 0.10) ) ;
-                color: var(--choise-chip-outlined-selected-text-color, var(--primary, #4caf50));
+                background-color: rgba( var(--input-chip-outlined-selected-bg-color-rgb, var(--primary-rgb, 76, 175, 80)), var(--state-active, 0.10) ) ;
+                color: var(--input-chip-outlined-selected-text-color, var(--primary, #4caf50));
                 border: solid 1px;
-                border-color: var(--choise-chip-outlined-selected-border-color, var(--on-background, #000000))
+                border-color: var(--input-chip-outlined-selected-border-color, var(--on-background, #000000))
             }
             
             :host([outlined][selected]:hover) {
-                background-color: rgba( var(--choise-chip-outlined-selected-hover-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-hover, 0.04) ) ;
+                background-color: rgba( var(--input-chip-outlined-selected-hover-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-hover, 0.04) ) ;
             }
             
             /* outlined selected choice chip when hovering */
             :host([selected]:hover) {
-                background-color: var(--choise-chip-selected-hover-bg-color, var(--primary, #4caf4f));
+                background-color: var(--input-chip-selected-hover-bg-color, var(--primary, #4caf4f));
             }
             
             /* hover */
             :host(:hover){
-                background-color: rgba( var(--choise-chip-unselected-hover-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-hover, 0.04) ) ;
+                background-color: rgba( var(--input-chip-unselected-hover-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-hover, 0.04) ) ;
             }
 
             /* unselected chip when pressing */
             :host:active {
-                background-color: rgba( var(--choise-chip-unselected-active-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-active, 0.10) ) ;
+                background-color: rgba( var(--input-chip-unselected-active-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-active, 0.10) ) ;
             }
 
             /* unselected chip when focusing */
             :host([focused]) {
-                background-color: rgba( var(--choise-chip-unselected-focus-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-focus, 0.12) ) ;
+                background-color: rgba( var(--input-chip-unselected-focus-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-focus, 0.12) ) ;
             }
 
             /* unselected chip when pressing */
             :host([selected]):active {
-                background-color: rgba( var(--choise-chip-selected-active-bg-color-rgb, var(--primary-rgb, 76, 175, 80)), var(--state-active, 0.10) ) ;
+                background-color: rgba( var(--input-chip-selected-active-bg-color-rgb, var(--primary-rgb, 76, 175, 80)), var(--state-active, 0.10) ) ;
             }
 
             /* selected chip when focusing */
             :host([selected][focused]) {
-                background-color: rgba( var(--choise-chip-selected-focus-bg-color-rgb, var(--primary-rgb, 76, 175, 80)), var(--state-focus, 0.12) ) ;
+                background-color: rgba( var(--input-chip-selected-focus-bg-color-rgb, var(--primary-rgb, 76, 175, 80)), var(--state-focus, 0.12) ) ;
             }
             
             /* disabled chip selected */
             :host([selected][disabled]){
-                color: var(--choise-chip-selected-color, var(--on-primary, #ffffff));
-                background-color: rgba( var(--choise-chip-disabled-selected-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
+                color: var(--input-chip-selected-color, var(--on-primary, #ffffff));
+                background-color: rgba( var(--input-chip-disabled-selected-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
             }
 
             /* disabled chip selected */
             :host([outlined][selected][disabled]){
-                color: var(--choise-chip-selected-color, var(--on-primary, #ffffff));
-                background-color: rgba( var(--choise-chip-disabled-selected-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
+                color: var(--input-chip-selected-color, var(--on-primary, #ffffff));
+                background-color: rgba( var(--input-chip-outlined-disabled-selected-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
             }
 
 
             :host([disabled]) {
                 cursor: default;
-                background-color: rgba( var(--choise-chip-disabled-selected-bg-color-rgb, var(--background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
+                background-color: rgba( var(--input-chip-disabled-selected-bg-color-rgb, var(--background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
             }
             /* disabled chip should have no ripple effect */
             :host([disabled]) furo-ripple{
@@ -315,13 +346,13 @@ class FuroChip extends FBP(LitElement) {
 
             /* disabled chip should have no hover effect */
             :host([selected][disabled]:hover){
-                background-color: rgba( var(--choise-chip-disabled-selected-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
+                background-color: rgba( var(--input-chip-disabled-selected-bg-color-rgb, var(--on-background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
             }
             
             
             /* disabled chip should have no hover effect */
             :host([disabled]:hover){
-                background-color: rgba( var(--choise-chip-disabled-selected-bg-color-rgb, var(--background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
+                background-color: rgba( var(--input-chip-disabled-selected-bg-color-rgb, var(--background-rgb, 33, 33, 33)), var(--state-disabled, 0.38) ) ;
             }
 
             
@@ -337,6 +368,7 @@ class FuroChip extends FBP(LitElement) {
       
             span {
                 margin: 0 5px;
+                line-height: 24px;
             }
       
             furo-icon {
@@ -352,7 +384,7 @@ class FuroChip extends FBP(LitElement) {
   render() {
     return html`
           ${this.leadingIcon? html` <furo-icon class="lead" icon="${this.leadingIcon}"></furo-icon> `:html``}
-          <input id="input" type="checkbox" ?selected=${this.selected}  ?autofocus=${this.autofocus} ?disabled=${this.disabled} 
+          <input id="input" type="checkbox" ?checked=${this.selected}  ?autofocus=${this.autofocus} ?disabled=${this.disabled} 
                  ƒ-focus="--focus" @-input="--inputInput(*)" @-focusout="--focusOutReceived" @-focus="--focusReceived" @-blur="-^blur"  >
           <span>${this.text}</span>
           ${this.trailingIcon? html` <furo-icon class="trail" icon="${this.trailingIcon}"></furo-icon>`:html``}
