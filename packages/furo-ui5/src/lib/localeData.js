5dfaea5805c499d5466d89a3c8b499ba78596443
fan zou <fan.zou@gmail.com>
2020-09-10 15:14:14 +0200

    chore: move CLDR from ui5.sap.com to project intern

---
+++ packages/furo-ui5/src/lib/localeData.js
@@ -0,0 +1,24 @@
+import { registerCldr, setCldrData } from "@ui5/webcomponents-base/dist/asset-registries/LocaleData.js";
+
+import de_CH from "../../assets/cldr/de_CH.js";
+
+
+const cldrData = {
+  de_CH};
+
+const allEntriesInlined = Object.entries(cldrData).every(([_key, value]) => typeof (value) === "object");
+
+if (allEntriesInlined) {
+  console.warn(`Inefficient bundling detected: consider bundling CLDR imports as URLs instead of inlining them.
+See rollup-plugin-url or webpack file-loader for more information.
+Suggested pattern: "assets\\\/.*\\\.json"`);
+}
+
+
+Object.entries(cldrData).forEach(([key, value]) => {
+  if (typeof (value) === "object") {
+    setCldrData(key, value);
+  } else {
+    registerCldr(key, value);
+  }
+});
