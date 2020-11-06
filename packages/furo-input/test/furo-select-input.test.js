29a9083f28b83623fb5a21197874beb49f804c6c
fan zou <fan.zou@gmail.com>
2020-11-06 14:39:15 +0100

    fix: fixes of number input. Fixes #105

--- packages/furo-input/test/furo-select-input.test.js
+++ packages/furo-input/test/furo-select-input.test.js
@@ -100,7 +100,7 @@ describe('furo-select-input', () => {
       done();
     });
     element._FBPTriggerWire('--inputInput', {
-      composedPath: () => [{ value: 1337, validity: {} }],
+      composedPath: () => [{ value: 1337, validity: {valid: true} }],
     });
   });
 
