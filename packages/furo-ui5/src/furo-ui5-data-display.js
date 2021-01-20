2944988f709444ad6d8e29bd3330af5f4224005e
maltenorstroem <malte.norstroem@gmail.com>
2021-01-20 15:07:45 +0100

    fix: word-wrap added

--- packages/furo-ui5/src/furo-ui5-data-display.js
+++ packages/furo-ui5/src/furo-ui5-data-display.js
@@ -74,6 +74,7 @@ class FuroUi5DataDisplay extends FBP(LitElement) {
           margin: 0;
           font-family: var(--sapFontFamily, '72');
           color: var(--sapTextcolor, '#32363a');
+          word-break: break-word;
         }
         :host([value-state='Information']) p {
           color: var(--sapInformativeColor, #0a6ed1);
