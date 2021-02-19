fa6cbd992eb6ca246edfa2f749b7a0f9c98adafe
fan zou <fan.zou@gmail.com>
2021-02-19 14:04:47 +0100

    chore: add imports to registry.

--- packages/furo-ui5/src/standard-type-renderers/display-registry.js
+++ packages/furo-ui5/src/standard-type-renderers/display-registry.js
@@ -31,6 +31,7 @@
  *
  */
 
+import './display-bool.js';
 import './display-string.js';
 import './display-int32.js';
 import './display-uint32.js';
@@ -41,9 +42,10 @@ import './display-float.js';
 
 import './display-furo-fat-string.js';
 import './display-furo-link.js';
+import './display-furo-reference.js';
 import './display-furo-type-money.js';
 import './display-furo-type-date.js';
-import './display-furo-reference.js';
 
+import './display-google-protobuf-timestamp.js';
 import './display-google-type-money.js';
 import './display-google-type-date.js';
