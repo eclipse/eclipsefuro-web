2c6b95190879268d3cc75feec1314e3c78d94d81
maltenorstroem <malte.norstroem@gmail.com>
2021-03-10 09:05:46 +0100

    feat: new column variants, some css fixes

--- packages/furo-form/demos/demos.js
+++ packages/furo-form/demos/demos.js
@@ -1,4 +1,5 @@
 import './demo-furo-collapsible-box.js';
 import './demo-furo-form-layouter.js';
 import './demo-furo-form-layouter-complex.js';
+import './demo-furo-form-layouter-variants.js';
 import './demo-furo-button-bar.js';
