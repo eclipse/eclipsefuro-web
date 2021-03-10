b25bdcddf3aa6c22b353915429dc770dfd70afad
fan zou <fan.zou@gmail.com>
2021-03-10 09:45:50 +0100

    chore: fix styling

--- packages/furo-ui5/src/standard-type-renderers/display-bool.js
+++ packages/furo-ui5/src/standard-type-renderers/display-bool.js
@@ -25,7 +25,6 @@ export class DisplayBool extends LitElement {
         :host {
           display: inline;
           word-break: keep-all;
-          text-align: left;
         }
 
         :host([tabular-form]) {
