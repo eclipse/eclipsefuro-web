5b6f765fcc863688daefdf1b5e136d24b0fb254a
veith <veithz@gmail.com>
2021-06-02 10:50:51 +0200

    fix: some tests fixed

--- packages/furo-ui5/test/furo-ui5-data-textarea-input.test.js
+++ packages/furo-ui5/test/furo-ui5-data-textarea-input.test.js
@@ -207,7 +207,7 @@ describe('furo-ui5-data-textarea-input', () => {
           'check valueStateMessage content',
         );
         done();
-      }, 100);
+      }, 30);
     });
 
     daoFat.injectRaw(testRecordMeta);
