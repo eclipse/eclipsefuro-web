ffdddb9de180ab0c34029c712a1f345a7a5cc831
veith <veithz@gmail.com>
2021-06-14 17:49:19 +0200

    feat: ref searcher label init

--- packages/furo-ui5/src/furo-ui5-data-reference-search-labeled.js
+++ packages/furo-ui5/src/furo-ui5-data-reference-search-labeled.js
@@ -21,12 +21,6 @@ class FuroUi5DataReferenceSearchLabeled extends FBP(LitElement) {
   constructor(props) {
     super(props);
     this.label = '';
-
-    this.subField = 'data';
-    this.displayField = 'display_name';
-    this.valueField = 'id';
-    this.valueSubField = 'id';
-    this.displaySubField = 'display_name';
   }
 
   /**
@@ -45,21 +39,6 @@ class FuroUi5DataReferenceSearchLabeled extends FBP(LitElement) {
     // this._FBPTraceWires();
   }
 
-  /**
-   * inject list
-   * @param arr
-   */
-  injectList(arr) {
-    this._FBPTriggerWire('--injectList', arr);
-  }
-
-  /**
-   * Inject the array of a collection
-   * @param entities
-   */
-  injectEntities(entities) {
-    this._FBPTriggerWire('--injectList', entities);
-  }
 
   static get properties() {
     return {
@@ -67,10 +46,15 @@ class FuroUi5DataReferenceSearchLabeled extends FBP(LitElement) {
        * the label for the data-reference-search
        */
       label: { type: String },
+
+      placeholder: { type: String },
       /**
        * A Boolean attribute which, if present, means this field cannot be edited by the user.
        */
       disabled: {
+        type: Boolean,
+      },
+      readonly: {
         type: Boolean,
       },
       /**
@@ -79,38 +63,8 @@ class FuroUi5DataReferenceSearchLabeled extends FBP(LitElement) {
       required: {
         type: Boolean,
       },
-      /**
-       * If you inject an array with complex objects, declare here the path where display_name and value_field are located.
-       *
-       * This is only needed if display_name and value_field are not located in the root of the object.
-       * @property sub-field
-       */
-      subField: { type: String, attribute: 'sub-field', reflect: true },
-      /**
-       * The name of the field from the injected collection that contains the label for the dropdown array.
-       * @property display-field
-       */
-      displayField: { type: String, attribute: 'display-field', reflect: true },
-      /**
-       * if you bind a complex type, declare here the field which gets updated of display_name by selecting an item.
-       * If you bind a scalar, you dont need this attribute.
-       * @property value-field
-       */
-      valueField: { type: String, attribute: 'value-field', reflect: true },
-      /**
-       * if you bind a complex type, declare here the field which gets updated of value by selecting an item.
-       *
-       * If you bind a scalar, you dont need this attribute.
-       * @property value-sub-field
-       */
-      valueSubField: { type: String, attribute: 'value-sub-field', reflect: true },
-      /**
-       * if you bind a complex type, declare here the field which gets updated of display_name by selecting an item.
-       *
-       * If you bind a scalar, you dont need this attribute.
-       * @property display-sub-field
-       */
-      displaySubField: { type: String, attribute: 'display-sub-field', reflect: true },
+
+
     };
   }
 
@@ -137,20 +91,7 @@ class FuroUi5DataReferenceSearchLabeled extends FBP(LitElement) {
     Ui5LabelDataBinding.bindData(this, fieldNode);
   }
 
-  /**
-   * inject collection
-   * @param arr
-   */
-  collectionIn(arr) {
-    this._FBPTriggerWire('--collectionIn', arr);
-  }
 
-  /**
-   * reset combobox
-   */
-  reset() {
-    this._FBPTriggerWire('--reset');
-  }
 
   /**
    * @private
@@ -161,21 +102,14 @@ class FuroUi5DataReferenceSearchLabeled extends FBP(LitElement) {
     return html`
       <furo-ui5-form-field-container>
         <ui5-label label slot="label" for="Input" show-colon ?required=${this.required}
-          >${this.label}</ui5-label
+        >${this.label}</ui5-label
         >
         <furo-ui5-data-reference-search
           content
           id="Input"
+          ?readonly=${this.readonly}
           ?disabled=${this.disabled}
-          sub-field="${this.subField}"
-          display-field="${this.displayField}"
-          value-field="${this.valueField}"
-          value-sub-field="${this.valueSubField}"
-          display-sub-field="${this.displaySubField}"
-          ƒ-inject-list="--injectList"
           ƒ-bind-data="--data"
-          ƒ-collection-in="--collectionIn"
-          ƒ-reset="--reset"
           ƒ-focus="--focus"
         ></furo-ui5-data-reference-search>
       </furo-ui5-form-field-container>
