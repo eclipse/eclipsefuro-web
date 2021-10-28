72a4ebb15042c116a53acd4e1f6f636ed08e3e33
veith <veithz@gmail.com>
2021-10-28 11:31:26 +0200

    chore: delete furo-timing
    Signed-off-by: veith <veithz@gmail.com>

--- packages/furo-timing/demos/demos.js
+++
@@ -1,2 +0,0 @@
-import './demo-furo-de-bounce.js';
-import './demo-furo-de-bounce-immediately.js';
--- packages/furo-timing/src/furo-catalog.js
+++
@@ -1,6 +0,0 @@
-import './furo-count-down.js';
-import './furo-de-bounce.js';
-import './furo-delay-event.js';
-import './furo-once.js';
-import './furo-interval-pulse.js';
-import './totman-switch.js';
