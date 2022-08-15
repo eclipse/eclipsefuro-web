133f27721e1c6f4bf0fa2a33d01bd45f48ff7222
veith <veithz@gmail.com>
2022-08-15 12:41:58 +0200

    fix: broadcast new-mc-data-injected event for the message-container displays

--- packages/furo-data/src/furo-message-container-handler.js
+++ packages/furo-data/src/furo-message-container-handler.js
@@ -1,5 +1,5 @@
-import { LitElement, css } from 'lit';
-import { NodeEvent } from '@furo/framework/src/EventTreeNode';
+import { LitElement, css } from 'lit'
+import { NodeEvent } from '@furo/framework/src/EventTreeNode'
 
 /**
  * `furo-message-container-handler`
@@ -21,12 +21,12 @@ import { NodeEvent } from '@furo/framework/src/EventTreeNode';
  */
 class FuroMessageContainerHandler extends LitElement {
   constructor() {
-    super();
+    super()
     /**
      * the Message Container Data Object
      * @private
      */
-    this.messageDO = {};
+    this.messageDO = {}
   }
 
   /**
@@ -38,21 +38,16 @@ class FuroMessageContainerHandler extends LitElement {
     setTimeout(() => {
       // used to *reset* the metas according to the spec
       this.mcDO.broadcastEvent(
-        new NodeEvent('before-new-data-inject', this.mcDO)
-      );
+        new NodeEvent('before-new-data-inject', this.mcDO),
+      )
       // this.mcDO broadcast will disable validation during setting the values
-      this.mcDO.broadcastEvent(new NodeEvent('disable-validation', this.mcDO));
+      this.mcDO.broadcastEvent(new NodeEvent('disable-validation', this.mcDO))
 
-      this.mcDO._rawEntity = messageContainer;
-      this.mcDO._value = messageContainer;
-      this.mcDO._pristine = true;
-      this.mcDO._isValid = true;
 
-      /**
-       * Broadcast Event
-       * this.mcDO will set all fields as pristine and end enable the validation
-       */
-      this.mcDO.broadcastEvent(new NodeEvent('new-data-injected', this.mcDO));
+      this.mcDO._value = messageContainer
+      this.mcDO._pristine = true
+      this.mcDO._isValid = true
+
 
       /**
        * @fires data-injected
@@ -65,172 +60,184 @@ class FuroMessageContainerHandler extends LitElement {
        * detail payload: **{NodeEvent}**
        */
       this.mcDO.dispatchNodeEvent(
-        new NodeEvent('data-injected', this.mcDO, false)
-      );
+        new NodeEvent('data-injected', this.mcDO, false),
+      )
 
-      if(this.rootNode){
-      // set empty state on all fields
-      this.rootNode.__childNodes.forEach(node => {
-        this.rootNode[node._name]._setState({
-          state: 'None',
-          description: '',
-          field: '',
-        });
-      });
+      if (this.rootNode) {
+        // set empty state on all fields
+        this.rootNode.__childNodes.forEach(node => {
+          this.rootNode[node._name]._setState({
+            state: 'None',
+            description: '',
+            field: '',
+          })
+        })
       }
 
-      if (messageContainer !== undefined) {
+      if (messageContainer !== undefined && messageContainer.details) {
         messageContainer.details.forEach(messageSet => {
           if (messageSet.fields) {
-            const messageDetails = messageSet.fields;
+            const messageDetails = messageSet.fields
             messageDetails.forEach(message => {
-              const path = message.field.split('.');
+              const path = message.field.split('.')
               if (path.length > 0) {
                 // rest wieder in message reinwerfen
                 // eslint-disable-next-line no-param-reassign
-                message.field = path.slice(1).join('.');
+                message.field = path.slice(1).join('.')
                 // eslint-disable-next-line no-param-reassign
                 message.state = this._getStateFromMessageType(
-                  messageSet['@type'].replace(/.*\//, '')
-                );
+                  messageSet['@type'].replace(/.*\//, ''),
+                )
                 if (this.rootNode && this.rootNode[path[0]]) {
-                  this.rootNode[path[0]]._setState(message);
+                  this.rootNode[path[0]]._setState(message)
                 } else {
                   // eslint-disable-next-line no-console
-                  console.warn('Unknown target field', path);
+                  console.warn('Unknown target field', path)
                 }
               }
-            });
+            })
           }
 
           if (messageSet.field_violations) {
-            const messageDetails = messageSet.field_violations;
+            const messageDetails = messageSet.field_violations
             messageDetails.forEach(message => {
-              const path = message.field.split('.');
+              const path = message.field.split('.')
               if (path.length > 0) {
                 // rest wieder in message reinwerfen
                 // eslint-disable-next-line no-param-reassign
-                message.field = path.slice(1).join('.');
+                message.field = path.slice(1).join('.')
                 // eslint-disable-next-line no-param-reassign
-                message.state = "Error"
+                message.state = 'Error'
 
                 if (this.rootNode && this.rootNode[path[0]]) {
-                  this.rootNode[path[0]]._setState(message);
+                  this.rootNode[path[0]]._setState(message)
                 } else {
                   // eslint-disable-next-line no-console
-                  console.warn('Unknown target field', path);
+                  console.warn('Unknown target field', path)
                 }
               }
-            });
+            })
           }
-        });
+        })
+      } else {
+        // no details
+        this.mcDO.details._value = []
       }
 
-      if(messageContainer.success){
+      /**
+       * Broadcast Event
+       * this.mcDO will set all fields as pristine and end enable the validation
+       *
+       * This event is used in the furo-ui5-message-container-display component
+       */
+      this.mcDO.broadcastEvent(new NodeEvent('new-mc-data-injected', this.mcDO))
+
+
+      if (messageContainer.success) {
         /**
          * @event success
          * Fired when success field was set on the received messagecontainer.
          */
-        const customEvent = new Event('success', {composed:true, bubbles: true});
+        const customEvent = new Event('success', { composed: true, bubbles: true })
         this.dispatchEvent(customEvent)
-      }else{
+      } else {
         /**
          * @event no-success
          * Fired when success field on the messagecontainer was not set or is set to false
          */
-        const customEvent = new Event('no-success', {composed:true, bubbles: true});
+        const customEvent = new Event('no-success', { composed: true, bubbles: true })
         this.dispatchEvent(customEvent)
       }
 
       // count items of each type
-      const errs = [];
-      const warn = [];
-      const success = [];
-      const info = [];
-      const confirm = [];
+      const errs = []
+      const warn = []
+      const success = []
+      const info = []
+      const confirm = []
 
       this.mcDO.details.repeats.forEach(item => {
-        const type = item['@type']._value.replace(/.*\//, '');
+        const type = item['@type']._value.replace(/.*\//, '')
         switch (type) {
           case 'furo.ErrorMessage':
-            errs.push(...item.fields.repeats);
-            break;
+            errs.push(...item.fields.repeats)
+            break
 
           case 'furo.WarningMessage':
-            warn.push(...item.fields.repeats);
-            break;
+            warn.push(...item.fields.repeats)
+            break
 
           case 'furo.SuccessMessage':
-            success.push(...item.fields.repeats);
-            break;
+            success.push(...item.fields.repeats)
+            break
 
           case 'furo.InformationMessage':
-            info.push(...item.fields.repeats);
-            break;
+            info.push(...item.fields.repeats)
+            break
 
           case 'furo.ConfirmationMessage':
-            confirm.push(item);
-            break;
+            confirm.push(item)
+            break
           default:
         }
-      });
+      })
 
-      if(confirm.length >0){
+      if (confirm.length > 0) {
         /**
          * @event has-confirmation
          * Fired when the message container contains any confirmation field
          * detail payload: list with all confirmation fields
          */
-        const customEvent = new Event('has-confirmation', {composed:true, bubbles: true});
-        customEvent.detail = confirm;
+        const customEvent = new Event('has-confirmation', { composed: true, bubbles: true })
+        customEvent.detail = confirm
         this.dispatchEvent(customEvent)
 
       }
 
-      if(errs.length >0){
+      if (errs.length > 0) {
         /**
          * @event has-errors
          * Fired when the message container contains any error field
          * detail payload: list with all error fields
          */
-        const customEvent = new Event('has-errors', {composed:true, bubbles: true});
-        customEvent.detail = errs;
+        const customEvent = new Event('has-errors', { composed: true, bubbles: true })
+        customEvent.detail = errs
         this.dispatchEvent(customEvent)
       }
-      if(warn.length >0){
+      if (warn.length > 0) {
         /**
          * @event has-warnings
          * Fired when the message container contains any error field
          * detail payload: list with all warning fields
          */
-        const customEvent = new Event('has-warnings', {composed:true, bubbles: true});
-        customEvent.detail = warn;
+        const customEvent = new Event('has-warnings', { composed: true, bubbles: true })
+        customEvent.detail = warn
         this.dispatchEvent(customEvent)
       }
 
-      if(success.length >0){
+      if (success.length > 0) {
         /**
          * @event has-success
          * Fired when the message container contains any error field
          * detail payload: list with all success fields
          */
-        const customEvent = new Event('has-success', {composed:true, bubbles: true});
-        customEvent.detail = success;
+        const customEvent = new Event('has-success', { composed: true, bubbles: true })
+        customEvent.detail = success
         this.dispatchEvent(customEvent)
       }
 
-      if(info.length >0){
+      if (info.length > 0) {
         /**
          * @event has-infos
          * Fired when the message container contains any error field
          * detail payload: list with all info fields
          */
-        const customEvent = new Event('has-infos', {composed:true, bubbles: true});
-        customEvent.detail = info;
+        const customEvent = new Event('has-infos', { composed: true, bubbles: true })
+        customEvent.detail = info
         this.dispatchEvent(customEvent)
       }
 
-    }, 1);
+    }, 1)
   }
 
   /**
@@ -242,8 +249,7 @@ class FuroMessageContainerHandler extends LitElement {
    * @param fieldNode {FieldNode} Messagecontainer fieldnode
    */
   bindMessageContainer(fieldNode) {
-    this.mcDO = fieldNode;
-
+    this.mcDO = fieldNode
   }
 
   /**
@@ -256,7 +262,7 @@ class FuroMessageContainerHandler extends LitElement {
    */
   bindRootNode(fieldNode) {
 
-    this.rootNode = fieldNode;
+    this.rootNode = fieldNode
   }
 
   /**
@@ -270,7 +276,7 @@ class FuroMessageContainerHandler extends LitElement {
       :host {
         display: none;
       }
-    `;
+    `
   }
 
   /**
@@ -283,24 +289,24 @@ class FuroMessageContainerHandler extends LitElement {
   _getStateFromMessageType(messageType) {
     switch (messageType) {
       case 'furo.ErrorMessage':
-        return 'Error';
+        return 'Error'
 
       case 'furo.WarningMessage':
-        return 'Warning';
+        return 'Warning'
 
       case 'furo.InformationMessage':
-        return 'Information';
+        return 'Information'
 
       case 'furo.SuccessMessage':
-        return 'Success';
+        return 'Success'
 
       default:
-        return undefined;
+        return undefined
     }
   }
 }
 
 window.customElements.define(
   'furo-message-container-handler',
-  FuroMessageContainerHandler
-);
+  FuroMessageContainerHandler,
+)
